2814ce1fc24933e82d65a38921cd78376457a28d
programarivm <info@programarivm.com>
2024-02-13 15:21:45 +0100

    Added route

--- src/Controller/ChessboardController.php
+++ src/Controller/ChessboardController.php
@@ -4,11 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Attribute\Route;
 
 class ChessboardController extends AbstractController
 {
-    #[Route('/chessboard', name: 'app_chessboard')]
     public function index(): JsonResponse
     {
         return $this->json([
