280671a1158191c4f57fa54b4a5bf91a34ea6ecb
programarivm <info@programarivm.com>
2024-02-14 11:55:27 +0100

    Setup AssetMapper

--- assets/js/chessboard.js
+++ assets/js/chessboard.js
@@ -1,30 +1,30 @@
-import {INPUT_EVENT_TYPE, Chessboard} from "../cm-chessboard/Chessboard.js";
-import {FEN} from "../cm-chessboard/model/Position.js"
-import {Markers} from "../cm-chessboard/extensions/markers/Markers.js"
+import {INPUT_EVENT_TYPE, Chessboard} from "cm-chessboard"
+import {FEN} from "../vendor/cm-chessboard/src/model/Position.js"
+import {Markers} from "../vendor/cm-chessboard/src/extensions/markers/Markers.js"
 
 window.board = new Chessboard(document.getElementById("board"), {
-    position: FEN.start,
-    assetsUrl: "../cm-chessboard/",
-    style: {pieces: {file: "pieces/staunty.svg"}},
-    extensions: [{class: Markers}]
+  position: FEN.start,
+  assetsUrl: "../cm-chessboard/",
+  style: {pieces: {file: "pieces/staunty.svg"}},
+  extensions: [{class: Markers}]
 })
 
 window.board.enableMoveInput(inputHandler)
 
 function inputHandler(event) {
-    console.log(event)
-    switch (event.type) {
-        case INPUT_EVENT_TYPE.moveInputStarted:
-            return true // false cancels move
-        case INPUT_EVENT_TYPE.validateMoveInput:
-            return true // false cancels move
-        case INPUT_EVENT_TYPE.moveInputCanceled:
-            break
-        case INPUT_EVENT_TYPE.moveInputFinished:
-            break
-        case INPUT_EVENT_TYPE.movingOverSquare:
-            break
-    }
+  console.log(event)
+  switch (event.type) {
+    case INPUT_EVENT_TYPE.moveInputStarted:
+      return true // false cancels move
+    case INPUT_EVENT_TYPE.validateMoveInput:
+      return true // false cancels move
+    case INPUT_EVENT_TYPE.moveInputCanceled:
+      break
+    case INPUT_EVENT_TYPE.moveInputFinished:
+      break
+    case INPUT_EVENT_TYPE.movingOverSquare:
+      break
+  }
 }
 
 const output = document.getElementById("output")
--- importmap.php
+++ importmap.php
@@ -1,19 +1,56 @@
 <?php
 
-/**
- * Returns the importmap for this application.
- *
- * - "path" is a path inside the asset mapper system. Use the
- *     "debug:asset-map" command to see the full list of paths.
- *
- * - "entrypoint" (JavaScript only) set to true for any module that will
- *     be used as an "entrypoint" (and passed to the importmap() Twig function).
- *
- * The "importmap:require" command can be used to add new entries to this file.
- */
 return [
     'app' => [
         'path' => './assets/app.js',
         'entrypoint' => true,
+    ],
+    'cm-chessboard' => [
+        'version' => '8.5.0',
+    ],
+    'cm-chessboard/src/extensions/accessibility/Accessibility' => [
+        'version' => '8.5.0',
+    ],
+    'cm-chessboard/src/extensions/accessibility/I18n' => [
+        'version' => '8.5.0',
+    ],
+    'cm-chessboard/src/extensions/arrows/Arrows' => [
+        'version' => '8.5.0',
+    ],
+    'cm-chessboard/src/extensions/auto-border-none/AutoBorderNone' => [
+        'version' => '8.5.0',
+    ],
+    'cm-chessboard/src/extensions/html-layer/HtmlLayer' => [
+        'version' => '8.5.0',
+    ],
+    'cm-chessboard/src/extensions/markers/Markers' => [
+        'version' => '8.5.0',
+    ],
+    'cm-chessboard/src/extensions/persistence/Persistence' => [
+        'version' => '8.5.0',
+    ],
+    'cm-chessboard/src/lib/Svg' => [
+        'version' => '8.5.0',
+    ],
+    'cm-chessboard/src/lib/Utils' => [
+        'version' => '8.5.0',
+    ],
+    'cm-chessboard/src/model/ChessboardState' => [
+        'version' => '8.5.0',
+    ],
+    'cm-chessboard/src/model/Extension' => [
+        'version' => '8.5.0',
+    ],
+    'cm-chessboard/src/model/Position' => [
+        'version' => '8.5.0',
+    ],
+    'cm-chessboard/src/view/ChessboardView' => [
+        'version' => '8.5.0',
+    ],
+    'cm-chessboard/src/view/PositionAnimationsQueue' => [
+        'version' => '8.5.0',
+    ],
+    'cm-chessboard/src/view/VisualMoveInput' => [
+        'version' => '8.5.0',
     ],
 ];
