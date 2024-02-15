f4e08ca456a29369ff7c050d873c055bc657b550
programarivm <info@programarivm.com>
2024-02-15 21:14:05 +0100

    Updated chessboard.js

--- assets/js/chessboard.js
+++ assets/js/chessboard.js
@@ -6,66 +6,6 @@ import {Chess} from "https://cdn.jsdelivr.net/npm/chess.mjs@1/src/chess.mjs/Ches
 
 const chess = new Chess()
 
-function inputHandler(event) {
-    console.log("inputHandler", event)
-    if(event.type === INPUT_EVENT_TYPE.movingOverSquare) {
-        return // ignore this event
-    }
-    if(event.type !== INPUT_EVENT_TYPE.moveInputFinished) {
-        event.chessboard.removeMarkers(MARKER_TYPE.dot)
-        event.chessboard.removeMarkers(MARKER_TYPE.bevel)
-    }
-    if (event.type === INPUT_EVENT_TYPE.moveInputStarted) {
-        const moves = chess.moves({square: event.squareFrom, verbose: true})
-        for (const move of moves) { // draw dots on possible squares
-            if (move.promotion && move.promotion !== "q") {
-                continue
-            }
-            if (event.chessboard.getPiece(move.to)) {
-                event.chessboard.addMarker(MARKER_TYPE.bevel, move.to)
-            } else {
-                event.chessboard.addMarker(MARKER_TYPE.dot, move.to)
-            }
-        }
-        return moves.length > 0
-    } else if (event.type === INPUT_EVENT_TYPE.validateMoveInput) {
-        const move = {from: event.squareFrom, to: event.squareTo, promotion: event.promotion}
-        const result = chess.move(move)
-        if (result) {
-            event.chessboard.state.moveInputProcess.then(() => { // wait for the move input process has finished
-                event.chessboard.setPosition(chess.fen(), true).then(() => { // update position, maybe castled and wait for animation has finished
-                    // makeEngineMove(event.chessboard)
-                })
-            })
-        } else {
-            // promotion?
-            let possibleMoves = chess.moves({square: event.squareFrom, verbose: true})
-            for (const possibleMove of possibleMoves) {
-                if (possibleMove.promotion && possibleMove.to === event.squareTo) {
-                    event.chessboard.showPromotionDialog(event.squareTo, COLOR.white, (result) => {
-                        console.log("promotion result", result)
-                        if (result.type === PROMOTION_DIALOG_RESULT_TYPE.pieceSelected) {
-                            chess.move({from: event.squareFrom, to: event.squareTo, promotion: result.piece.charAt(1)})
-                            event.chessboard.setPosition(chess.fen(), true)
-                            // makeEngineMove(event.chessboard)
-                        } else {
-                            // promotion canceled
-                            event.chessboard.enableMoveInput(inputHandler, COLOR.white)
-                            event.chessboard.setPosition(chess.fen(), true)
-                        }
-                    })
-                    return true
-                }
-            }
-        }
-        return result
-    } else if (event.type === INPUT_EVENT_TYPE.moveInputFinished) {
-        if(event.legalMove) {
-            event.chessboard.disableMoveInput()
-        }
-    }
-}
-
 const board = new Chessboard(document.getElementById("board"), {
   position: chess.fen(),
   assetsUrl: "https://cdn.jsdelivr.net/npm/cm-chessboard@8.5.0/assets/",
@@ -78,4 +18,51 @@ const board = new Chessboard(document.getElementById("board"), {
   ]
 })
 
-board.enableMoveInput(inputHandler, COLOR.white)
+board.enableMoveInput(inputHandler)
+
+function inputHandler(event) {
+  if (event.type === INPUT_EVENT_TYPE.movingOverSquare) {
+    return // ignore this event
+  }
+
+  if (event.type !== INPUT_EVENT_TYPE.moveInputFinished) {
+    event.chessboard.removeMarkers(MARKER_TYPE.dot)
+    event.chessboard.removeMarkers(MARKER_TYPE.bevel)
+  }
+
+  if (event.type === INPUT_EVENT_TYPE.moveInputStarted) {
+    const moves = chess.moves({square: event.squareFrom, verbose: true})
+    for (const move of moves) { // draw dots on possible squares
+      if (move.promotion && move.promotion !== "q") {
+        continue
+      }
+      if (event.chessboard.getPiece(move.to)) {
+        event.chessboard.addMarker(MARKER_TYPE.bevel, move.to)
+      } else {
+        event.chessboard.addMarker(MARKER_TYPE.dot, move.to)
+      }
+    }
+    return moves.length > 0
+  } else if (event.type === INPUT_EVENT_TYPE.validateMoveInput) {
+    const move = {from: event.squareFrom, to: event.squareTo, promotion: event.promotion}
+    const result = chess.move(move)
+    if (!result) {
+      let possibleMoves = chess.moves({square: event.squareFrom, verbose: true})
+      for (const possibleMove of possibleMoves) {
+        if (possibleMove.promotion && possibleMove.to === event.squareTo) {
+          event.chessboard.showPromotionDialog(event.squareTo, event.piece.charAt(0), (result) => {
+            if (result.type === PROMOTION_DIALOG_RESULT_TYPE.pieceSelected) {
+              chess.move({from: event.squareFrom, to: event.squareTo, promotion: result.piece.charAt(1)})
+              event.chessboard.setPosition(chess.fen(), true)
+            } else {
+              chess.move({from: event.squareFrom, to: event.squareTo, promotion: 'q'})
+              event.chessboard.setPosition(chess.fen(), true)
+            }
+          })
+          return true
+        }
+      }
+    }
+    return result
+  }
+}
