7c6725e893c14cbf5605e4e879fdc526d953366f
programarivm <info@programarivm.com>
2024-02-22 17:14:16 +0100

    Refactored assets/apps.js

--- assets/app.js
+++ assets/app.js
@@ -7,11 +7,8 @@ const openingsSanMovetextModal = document.getElementById('openingsSanMovetextMod
 
 const openingsNameModal = document.getElementById('openingsNameModal');
 
-openingsSanMovetextModal.getElementsByTagName('form')[0].addEventListener('submit', event => {
-  event.preventDefault();
-  const formData = new FormData(openingsSanMovetextModal.getElementsByTagName('form')[0]);
-  const openings = Opening.byMovetext(formData.get('movetext'));
-  openingsSanMovetextModal.getElementsByTagName('tbody')[0].replaceChildren();
+const openingsTableDomNode = (openings, tbody) => {
+  tbody.replaceChildren();
   openings.forEach(opening => {
     const tr = document.createElement('tr');
     const ecoTd = document.createElement('td');
@@ -39,42 +36,22 @@ openingsSanMovetextModal.getElementsByTagName('form')[0].addEventListener('submi
       );
       window.location.href = '/openings';
     });
-    openingsSanMovetextModal.getElementsByTagName('tbody')[0].appendChild(tr);
+    tbody.appendChild(tr);
   });
+};
+
+openingsSanMovetextModal.getElementsByTagName('form')[0].addEventListener('submit', event => {
+  event.preventDefault();
+  const formData = new FormData(openingsSanMovetextModal.getElementsByTagName('form')[0]);
+  const openings = Opening.byMovetext(formData.get('movetext'));
+  const tbody = openingsSanMovetextModal.getElementsByTagName('tbody')[0];
+  openingsTableDomNode(openings, tbody);
 });
 
 openingsNameModal.getElementsByTagName('form')[0].addEventListener('submit', event => {
   event.preventDefault();
   const formData = new FormData(openingsNameModal.getElementsByTagName('form')[0]);
   const openings = Opening.byName(formData.get('name'));
-  openingsNameModal.getElementsByTagName('tbody')[0].replaceChildren();
-  openings.forEach(opening => {
-    const tr = document.createElement('tr');
-    const ecoTd = document.createElement('td');
-    const ecoText = document.createTextNode(opening.eco);
-    const nameTd = document.createElement('td');
-    const nameText = document.createTextNode(opening.name);
-    const movetextTd = document.createElement('td');
-    const movetextText = document.createTextNode(opening.movetext);
-    ecoTd.appendChild(ecoText);
-    nameTd.appendChild(nameText);
-    movetextTd.appendChild(movetextText);
-    tr.appendChild(ecoTd);
-    tr.appendChild(nameTd);
-    tr.appendChild(movetextTd);
-    tr.addEventListener('click', event => {
-      localStorage.setItem(
-        'command',
-        JSON.stringify({
-          variant: 'classical',
-          mode: 'san',
-          add: {
-            movetext: opening.movetext
-          }
-        })
-      );
-      window.location.href = '/openings';
-    });
-    openingsNameModal.getElementsByTagName('tbody')[0].appendChild(tr);
-  });
+  const tbody = openingsNameModal.getElementsByTagName('tbody')[0];
+  openingsTableDomNode(openings, tbody);
 });
