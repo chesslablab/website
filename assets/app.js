4068f06ff409961680ef6181fa063f3a78c6eab6
programarivm <info@programarivm.com>
2024-02-22 16:50:35 +0100

    Implemented opening search by movetext

--- assets/app.js
+++ assets/app.js
@@ -3,6 +3,45 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/app.css';
 import { Opening } from "https://cdn.jsdelivr.net/npm/@chesslablab/jsblab@0.0.8/src/index.min.js";
 
+const openingsSanMovetextModalForm = document.querySelector('#openingsSanMovetextModal form');
+const openingsSanMovetextModalTable = document.querySelector('#openingsSanMovetextModal table tbody');
+
+openingsSanMovetextModalForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const formData = new FormData(openingsSanMovetextModalForm);
+  const openings = Opening.byMovetext(formData.get('movetext'));
+  openingsSanMovetextModalTable.replaceChildren();
+  openings.forEach(opening => {
+    const tr = document.createElement('tr');
+    const ecoTd = document.createElement('td');
+    const ecoText = document.createTextNode(opening.eco);
+    const nameTd = document.createElement('td');
+    const nameText = document.createTextNode(opening.name);
+    const movetextTd = document.createElement('td');
+    const movetextText = document.createTextNode(opening.movetext);
+    ecoTd.appendChild(ecoText);
+    nameTd.appendChild(nameText);
+    movetextTd.appendChild(movetextText);
+    tr.appendChild(ecoTd);
+    tr.appendChild(nameTd);
+    tr.appendChild(movetextTd);
+    tr.addEventListener('click', event => {
+      localStorage.setItem(
+        'command',
+        JSON.stringify({
+          variant: 'classical',
+          mode: 'san',
+          add: {
+            movetext: opening.movetext
+          }
+        })
+      );
+      window.location.href = '/openings';
+    });
+    openingsSanMovetextModalTable.appendChild(tr);
+  });
+});
+
 const openingsNameModalForm = document.querySelector('#openingsNameModal form');
 const openingsNameModalTable = document.querySelector('#openingsNameModal table tbody');
 
