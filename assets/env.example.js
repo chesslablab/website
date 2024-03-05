858b508dfc582021900d5adacf67daf9ff80c124
programarivm <info@programarivm.com>
2024-03-05 18:35:49 +0100

    Updated assets/env.example.js

--- assets/env.example.js
+++ assets/env.example.js
@@ -1,9 +1,20 @@
-export const API_SCHEME = 'https';
-export const API_HOST = 'api.chesslablab.org';
-export const API_PORT = '443';
-export const API_VERSION = 'v1';
-export const API_KEY = '\$2y\$10\$D7TW0EU7ZOxTpHlN97vceeoJrCQ1pIJeDqsRJj3t.Z\/Dc91gggBXO';
+const API_SCHEME = 'https';
+const API_HOST = 'api.chesslablab.org';
+const API_PORT = '443';
+const API_VERSION = 'v1';
+const API_KEY = '\$2y\$10\$D7TW0EU7ZOxTpHlN97vceeoJrCQ1pIJeDqsRJj3t.Z\/Dc91gggBXO';
 
-export const WEBSOCKET_SCHEME = 'wss';
-export const WEBSOCKET_HOST = 'async.chesslablab.org';
-export const WEBSOCKET_PORT = '8443';
+const WEBSOCKET_SCHEME = 'wss';
+const WEBSOCKET_HOST = 'async.chesslablab.org';
+const WEBSOCKET_PORT = '8443';
+
+export {
+  API_SCHEME,
+  API_HOST,
+  API_PORT,
+  API_VERSION,
+  API_KEY,
+  WEBSOCKET_SCHEME,
+  WEBSOCKET_HOST,
+  WEBSOCKET_PORT
+};
