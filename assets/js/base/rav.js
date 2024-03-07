4ea949e3ccde39010c068a2ce092d0dfaf00bcfd
programarivm <info@programarivm.com>
2024-03-07 16:14:46 +0100

    Initialized rav

--- assets/js/base/rav.js
+++ assets/js/base/rav.js
@@ -1,7 +1,11 @@
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  databaseAnnotatedGames
+} from './rav_init.js';
 import '../../styles/app.css';
-// ...
 import * as env from '../../env.js';
+import * as mode from '../../mode.js';
+import * as variant from '../../variant.js';
 
 console.log('To do');
---
+++ assets/js/base/rav_init.js
@@ -0,0 +1,6 @@
+import Modal from 'bootstrap/js/dist/modal.js';
+
+export const databaseAnnotatedGames = {
+  modal: new Modal(document.getElementById('databaseAnnotatedGamesModal')),
+  form: document.querySelector('#databaseAnnotatedGamesModal form')
+}
