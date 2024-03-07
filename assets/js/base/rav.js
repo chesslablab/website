e3ecfefc47a67bbe1e8b817a95148c22cce251a6
programarivm <info@programarivm.com>
2024-03-07 13:21:44 +0100

    Updated assets/js/base/rav.js

--- assets/js/base/rav.js
+++ assets/js/base/rav.js
@@ -1,1 +1,7 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../styles/app.css';
+// ...
+import * as env from '../../env.js';
+
 console.log('To do');
