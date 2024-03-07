be84528a059dc3e688ae6f372bc06ead12f92132
programarivm <info@programarivm.com>
2024-03-07 19:25:06 +0100

    Fetch data from the API

--- assets/js/database/annotated_games/index.js
+++ assets/js/database/annotated_games/index.js
@@ -2,7 +2,19 @@ import {
   databaseAnnotatedGames,
   ravMovesTable
 } from '../../../js/base/rav/init.js';
-
-databaseAnnotatedGames.modal.show();
+import * as env from '../../../env.js';
 
 ravMovesTable.domElem();
+
+await fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/annotations/games`, {
+  method: 'GET',
+  headers: {
+    'X-Api-Key': `${env.API_KEY}`
+  }
+})
+.then(res => res.json())
+.then(res => {
+  databaseAnnotatedGames.modal.show();
+  console.log(res);
+  // TODO
+});
