485f985bd13e56a5a1ec40232242da101723cdf1
programarivm <info@programarivm.com>
2024-03-12 10:56:54 +0100

    Updated to @chesslablab/jsblab 0.1.4

--- importmap.php
+++ importmap.php
@@ -38,7 +38,7 @@ return [
         'version' => '0.0.1',
     ],
     '@chesslablab/jsblab' => [
-        'version' => '0.1.3',
+        'version' => '0.1.4',
     ],
     'bootstrap' => [
         'version' => '5.3.2',
