a70f7d3a8a61150107a42c4a8bce0a7e63c38586
programarivm <info@programarivm.com>
2024-03-13 18:07:10 +0100

    Implemented progress bar

---
+++ assets/js/layout/progress.js
@@ -0,0 +1,7 @@
+import Modal from 'bootstrap/js/dist/modal.js';
+
+const progress = {
+  modal: new Modal(document.getElementById('progressModal'))
+}
+
+export default progress;
--- assets/js/layout/rav/init.js
+++ assets/js/layout/rav/init.js
@@ -1,3 +1,4 @@
 import databaseAnnotatedGames from './databaseAnnotatedGames.js';
 import historyButtons from './historyButtons.js';
 import gameActionsDropdown from './gameActionsDropdown.js';
+import progress from '../progress.js';
