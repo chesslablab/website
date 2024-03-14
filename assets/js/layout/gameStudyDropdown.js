02f04e2300bee9ae35e6d5ee1d2aa4088216b2c0
programarivm <info@programarivm.com>
2024-03-14 18:21:24 +0100

    Display an indeterminate linear progress bar

--- assets/js/layout/gameStudyDropdown.js
+++ assets/js/layout/gameStudyDropdown.js
@@ -1,4 +1,5 @@
 import chessboard from './chessboard.js';
+import progressModal from './progressModal.js';
 import sanMovesTable from './sanMovesTable.js';
 import * as env from '../../env.js';
 import * as variant from '../../variant.js';
@@ -7,6 +8,7 @@ const gameStudyDropdown = document.querySelector('#gameStudyDropdown ul');
 
 gameStudyDropdown.children.item(0).addEventListener('click', async (event) => {
   event.preventDefault();
+  progressModal.modal.show();
   await fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/download/image`, {
     method: 'POST',
     headers: {
@@ -27,11 +29,18 @@ gameStudyDropdown.children.item(0).addEventListener('click', async (event) => {
     document.body.appendChild(a);
     a.click();
     a.remove();
+  })
+  .catch(error => {
+    // TODO
+  })
+  .finally(() => {
+    progressModal.modal.hide();
   });
 });
 
 gameStudyDropdown.children.item(1).addEventListener('click', async (event) => {
   event.preventDefault();
+  progressModal.modal.show();
   await fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/download/mp4`, {
     method: 'POST',
     headers: {
@@ -54,6 +63,12 @@ gameStudyDropdown.children.item(1).addEventListener('click', async (event) => {
     document.body.appendChild(a);
     a.click();
     a.remove();
+  })
+  .catch(error => {
+    // TODO
+  })
+  .finally(() => {
+    progressModal.modal.hide();
   });
 });
 
