e23cc139d1f437ccb4269e29ffd4b53fc3d22254
programarivm <info@programarivm.com>
2024-03-15 19:05:37 +0100

    Set input to false

--- assets/js/pages/play/online/index.js
+++ assets/js/pages/play/online/index.js
@@ -1,4 +1,8 @@
 import ws from '../../../layout/play/ws.js';
+import chessboard from '../../../layout/chessboard.js';
+
+chessboard.state.inputWhiteEnabled = false;
+chessboard.state.inputBlackEnabled = false;
 
 await ws.connect();
 
