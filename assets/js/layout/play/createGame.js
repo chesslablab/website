a68b53dbc7b733f5f16d2c66a34a4b81ed28b6a8
programarivm <info@programarivm.com>
2024-03-15 17:57:28 +0100

    Cleanup

--- assets/js/layout/play/createGame.js
+++ assets/js/layout/play/createGame.js
@@ -1,42 +1,8 @@
 import Modal from 'bootstrap/js/dist/modal.js';
-import ws from './ws.js';
 
 const createGame = {
   modal: new Modal(document.getElementById('createGameModal')),
-  form: document.querySelector('#createGameModal form'),
-  domElem: (games) => {
-    if (games.length > 0) {
-      const tbody = document.querySelector('#createGameModal table tbody')
-      tbody.replaceChildren();
-      games.forEach(game => {
-        const tr = document.createElement('tr');
-        const usernameTd = document.createElement('td');
-        const usernameText = document.createTextNode('Guest');
-        const minTd = document.createElement('td');
-        const minText = document.createTextNode(game.min);
-        const incrementTd = document.createElement('td');
-        const incrementText = document.createTextNode(game.increment);
-        const colorTd = document.createElement('td');
-        const colorText = document.createTextNode(game.color);
-        const variantTd = document.createElement('td');
-        const variantText = document.createTextNode(game.variant);
-        usernameTd.appendChild(usernameText);
-        minTd.appendChild(minText);
-        incrementTd.appendChild(incrementText);
-        colorTd.appendChild(colorText);
-        variantTd.appendChild(variantText);
-        tr.appendChild(usernameTd);
-        tr.appendChild(minTd);
-        tr.appendChild(incrementTd);
-        tr.appendChild(colorTd);
-        tr.appendChild(variantTd);
-        tr.addEventListener('click', () => {
-          ws.send(`/accept ${game.hash}`);
-        });
-        tbody.appendChild(tr);
-      });
-    }
-  }
+  form: document.querySelector('#createGameModal form')
 }
 
 export default createGame;
--- assets/js/pages/play/online/index.js
+++ assets/js/pages/play/online/index.js
@@ -1,4 +1,3 @@
-import createGame from '../../../layout/play/createGame.js';
 import ws from '../../../layout/play/ws.js';
 
 await ws.connect();
