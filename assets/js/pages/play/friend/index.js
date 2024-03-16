16ca03a8208ecd603131311ebd92c87fdd7a3f3f
programarivm <info@programarivm.com>
2024-03-16 16:49:26 +0100

    Cleanup

--- assets/js/pages/play/friend/index.js
+++
@@ -1,7 +0,0 @@
-import copyInviteCodeModal from '../../../layout/play/copyInviteCodeModal.js';
-import playFriendModal from '../../../layout/play/playFriendModal.js';
-import ws from '../../../layout/play/ws.js';
-
-await ws.connect();
-
-playFriendModal.modal.show();
