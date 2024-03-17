4aa5624c40b0a11348938404daf02f1349c318e0
programarivm <info@programarivm.com>
2024-03-18 00:48:45 +0100

    Renamed openingsEcoCode as openingsEcoCodeModal

--- assets/js/pages/openings/eco_code/index.js
+++ assets/js/pages/openings/eco_code/index.js
@@ -1,4 +1,4 @@
-import openingsEcoCode from './openingsEcoCode.js';
+import openingsEcoCodeModal from './openingsEcoCodeModal.js';
 import gameActionsDropdown from '../../../pages/gameActionsDropdown.js';
 import gameStudyDropdown from '../../../pages/gameStudyDropdown.js';
 import historyButtons from '../../../pages/historyButtons.js';
@@ -8,4 +8,4 @@ await ws.connect();
 
 localStorage.clear();
 
-openingsEcoCode.modal.show();
+openingsEcoCodeModal.modal.show();
--- assets/js/pages/openings/eco_code/openingsEcoCodeModal.js
+++ assets/js/pages/openings/eco_code/openingsEcoCodeModal.js
@@ -2,16 +2,16 @@ import Modal from 'bootstrap/js/dist/modal.js';
 import { Opening } from '@chesslablab/jsblab';
 import openingsTable from '../openingsTable.js';
 
-const openingsEcoCode = {
+const openingsEcoCodeModal = {
   modal: new Modal(document.getElementById('openingsEcoCodeModal')),
   form: document.querySelector('#openingsEcoCodeModal form')
 }
 
-openingsEcoCode.form.getElementsByTagName('select')[0].addEventListener('change', event => {
+openingsEcoCodeModal.form.getElementsByTagName('select')[0].addEventListener('change', event => {
   event.preventDefault();
   const openings = Opening.byEco(event.target.value);
-  const tbody = openingsEcoCode.form.getElementsByTagName('tbody')[0];
-  openingsTable(openingsEcoCode.modal, openings, tbody);
+  const tbody = openingsEcoCodeModal.form.getElementsByTagName('tbody')[0];
+  openingsTable(openingsEcoCodeModal.modal, openings, tbody);
 });
 
-export default openingsEcoCode;
+export default openingsEcoCodeModal;
