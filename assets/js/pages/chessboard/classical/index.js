56549ee02ada26a5abf0eb439015e9396838dbce
programarivm <info@programarivm.com>
2024-03-18 16:13:29 +0100

    Fixed js/pages/chessboard/classical/index.js

--- assets/js/pages/chessboard/classical/index.js
+++ assets/js/pages/chessboard/classical/index.js
@@ -1,4 +1,10 @@
+import gameActionsDropdown from '../../../pages/gameActionsDropdown.js';
+import gameStudyDropdown from '../../../pages/gameStudyDropdown.js';
+import historyButtons from '../../../pages/historyButtons.js';
 import ws from '../../../fenWs.js';
 
 await ws.connect();
+
+localStorage.clear();
+
 ws.send('/start classical fen');
