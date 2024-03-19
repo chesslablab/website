6e040307f51d6dee6830e8e5282b283c49ae9fe6
programarivm <info@programarivm.com>
2024-03-19 16:39:20 +0100

    Implemented blackAutocomplete

---
+++ assets/js/pages/blackAutocomplete.js
@@ -0,0 +1,44 @@
+import Fuse from 'fuse.js';
+import * as env from '../../env.js';
+
+const blackAutocomplete = {
+  input: document.querySelector('input[name="Black"]'),
+  values: async () => {
+    let response = await fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/autocomplete/player`, {
+      method: 'GET',
+      headers: {
+        'X-Api-Key': `${env.API_KEY}`
+      }
+    });
+
+    return await response.json();
+  },
+  suggest: (result) => {
+    const suggestions = document.querySelector('input[name="Black"] + ul');
+    suggestions.classList.remove('d-none');
+    suggestions.replaceChildren();
+    result.forEach(item => {
+      const li = document.createElement('li');
+      const liText = document.createTextNode(item.item);
+      li.appendChild(liText);
+      li.addEventListener('click', () => {
+        document.querySelector('input[name="Black"]').value = item.item;
+        suggestions.classList.add('d-none');
+      });
+      suggestions.append(li);
+    });
+  }
+}
+
+const events = await blackAutocomplete.values();
+
+blackAutocomplete.input.addEventListener('change', async (event) => {
+  blackAutocomplete.input.value = event.target.value;
+});
+
+blackAutocomplete.input.addEventListener('keyup', (event) => {
+  const fuse = new Fuse(events);
+  blackAutocomplete.suggest(fuse.search(event.target.value, { limit: 10 }));
+});
+
+export default blackAutocomplete;
--- assets/js/pages/database/search_games/searchGamesModal.js
+++ assets/js/pages/database/search_games/searchGamesModal.js
@@ -1,6 +1,7 @@
 import Modal from 'bootstrap/js/dist/modal.js';
 import eventAutocomplete from '../../eventAutocomplete.js';
 import whiteAutocomplete from '../../whiteAutocomplete.js';
+import blackAutocomplete from '../../blackAutocomplete.js';
 
 const searchGamesModal = {
   modal: new Modal(document.getElementById('searchGamesModal')),
