39fb082b1fd87a2ac8e5e5aa2c9e260c30bed8c4
programarivm <info@programarivm.com>
2024-03-19 13:13:47 +0100

    Fuzzy search

--- assets/js/pages/eventAutocomplete.js
+++ assets/js/pages/eventAutocomplete.js
@@ -1,29 +1,32 @@
+import Fuse from 'fuse.js';
 import * as env from '../../env.js';
-
-const fetchEvents = async () => {
-  let response = await fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/autocomplete/event`, {
-    method: 'GET',
-    headers: {
-      'X-Api-Key': `${env.API_KEY}`
-    }
-  });
-
-  return await response.json();
-}
 
 const eventAutocomplete = {
   input: document.querySelector('input[name="Event"]'),
-  values: fetchEvents()
+  values: async () => {
+    let response = await fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/autocomplete/event`, {
+      method: 'GET',
+      headers: {
+        'X-Api-Key': `${env.API_KEY}`
+      }
+    });
+
+    return await response.json();
+  },
+  suggest: (result) => {
+    console.log(result);
+  }
 }
+
+const events = await eventAutocomplete.values();
 
 eventAutocomplete.input.addEventListener('change', async (event) => {
   console.log(event.target.value);
-  const foobar = await eventAutocomplete.values;
-  console.log(foobar);
 });
 
 eventAutocomplete.input.addEventListener('keyup', (event) => {
-  console.log(event.target.value);
+  const fuse = new Fuse(events);
+  eventAutocomplete.suggest(fuse.search(event.target.value, { limit: 20 }));
 });
 
 export default eventAutocomplete;
