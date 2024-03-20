ab6130ae53baa445ad91235fb60ec34d3292f750
programarivm <info@programarivm.com>
2024-03-20 10:47:55 +0100

    Updated API_KEY

--- assets/env.example.js
+++ assets/env.example.js
@@ -2,7 +2,7 @@ const API_SCHEME = 'https';
 const API_HOST = 'api.chesslablab.org';
 const API_PORT = '443';
 const API_VERSION = 'v1';
-const API_KEY = '\$2y\$06\$3TtLPlyViUQbpOf76guM\/eahrQmVnGxcZ8embmR8t13mMO8T07.DS';
+const API_KEY = '\$2y\$04\$k31GoY4Zgq\/xAFiQO5H63.C.TrOgjc72CZgOEonWhYhhTvhrHruUm';
 
 const WEBSOCKET_SCHEME = 'wss';
 const WEBSOCKET_HOST = 'async.chesslablab.org';
