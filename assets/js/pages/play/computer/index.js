452bb09039e75819eea91bb0d7a0a697b82b2db4
programarivm <info@programarivm.com>
2024-03-21 19:20:07 +0100

    Deleted gameStudyDropdown

--- assets/js/pages/play/computer/index.js
+++ assets/js/pages/play/computer/index.js
@@ -1,6 +1,5 @@
 import playComputerModal from './playComputerModal.js';
 import gameActionsDropdown from '../../../pages/gameActionsDropdown.js';
-import gameStudyDropdown from '../../../pages/gameStudyDropdown.js';
 import historyButtons from '../../../pages/historyButtons.js';
 import ws from '../../../stockfishWs.js';
 
