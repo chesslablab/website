30ad3748ff81c19f7f0af21018c8ed1c4f49df15
programarivm <info@programarivm.com>
2024-03-23 00:13:07 +0100

    Updated controller

--- src/Controller/Pages/Learn/AnnotatedGamesController.php
+++ src/Controller/Pages/Learn/AnnotatedGamesController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Pages\Database;
+namespace App\Controller\Pages\Learn;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
