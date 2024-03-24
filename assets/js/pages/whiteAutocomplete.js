225c9d6bad0b48908b05296bbbefd94041a79e39
programarivm <info@programarivm.com>
2024-03-24 15:28:12 +0100

    Characters length limit

--- assets/js/pages/whiteAutocomplete.js
+++ assets/js/pages/whiteAutocomplete.js
@@ -9,36 +9,38 @@ whiteAutocomplete.input.addEventListener('change', (event) => {
 });
 
 whiteAutocomplete.input.addEventListener('keyup', (event) => {
-  fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/autocomplete/player`, {
-    method: 'POST',
-    headers: {
-      'X-Api-Key': `${env.API_KEY}`
-    },
-    body: JSON.stringify({
-      White: event.target.value
+  if (event.target.value.length % 2 === 0) {
+    fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/autocomplete/player`, {
+      method: 'POST',
+      headers: {
+        'X-Api-Key': `${env.API_KEY}`
+      },
+      body: JSON.stringify({
+        White: event.target.value
+      })
     })
-  })
-  .then(res => res.json())
-  .then(res => {
-    const suggestions = document.querySelector('input[name="White"] + ul');
-    suggestions.classList.remove('d-none');
-    suggestions.replaceChildren();
-    res.forEach(item => {
-      const li = document.createElement('li');
-      li.appendChild(document.createTextNode(item));
-      li.addEventListener('click', () => {
-        document.querySelector('input[name="White"]').value = item;
-        suggestions.classList.add('d-none');
+    .then(res => res.json())
+    .then(res => {
+      const suggestions = document.querySelector('input[name="White"] + ul');
+      suggestions.classList.remove('d-none');
+      suggestions.replaceChildren();
+      res.forEach(item => {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(item));
+        li.addEventListener('click', () => {
+          document.querySelector('input[name="White"]').value = item;
+          suggestions.classList.add('d-none');
+        });
+        suggestions.append(li);
       });
-      suggestions.append(li);
+    })
+    .catch(error => {
+      // TODO
+    })
+    .finally(() => {
+      // TODO
     });
-  })
-  .catch(error => {
-    // TODO
-  })
-  .finally(() => {
-    // TODO
-  });
+  }
 });
 
 export default whiteAutocomplete;
