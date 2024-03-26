b6e79583c89558a071ebf4e0aea6614c3cd8f8a8
programarivm <info@programarivm.com>
2024-03-26 10:52:05 +0100

    Updated topOpeningsModal

--- assets/js/pages/database/top_openings/topOpeningsModal.js
+++ assets/js/pages/database/top_openings/topOpeningsModal.js
@@ -1,19 +1,58 @@
 import { Chart, registerables } from 'https://cdn.jsdelivr.net/npm/chart.js@4.4.2/+esm';
 import Modal from 'bootstrap/js/dist/modal.js';
+import movesMetadataTable from '../../movesMetadataTable.js';
+import progressModal from '../../progressModal.js';
+import ws from '../../../sanWs.js';
+import * as env from '../../../../env.js';
+import * as mode from '../../../../mode.js';
 
 Chart.register(...registerables);
+
+const handleBarClick = (event, clickedElements) => {
+  progressModal.modal.show();
+  if (clickedElements.length === 0) {
+    return;
+  }
+  const { dataIndex, raw } = clickedElements[0].element.$context;
+  fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/search`, {
+    method: 'POST',
+    headers: {
+      'X-Api-Key': `${env.API_KEY}`
+    },
+    body: JSON.stringify({
+      Result: event.chart.data.datasets[0].label,
+      ECO: event.chart.data.labels[dataIndex]
+    })
+  })
+  .then(res => res.json())
+  .then(res => {
+    movesMetadataTable.props = res[0];
+    movesMetadataTable.mount();
+    const add = {
+      movetext: res[0].movetext
+    };
+    ws.send(`/start classical ${mode.SAN} "${JSON.stringify(add).replace(/"/g, '\\"')}"`);
+  })
+  .catch(error => {
+    // TODO
+  })
+  .finally(() => {
+    topOpeningsModal.modal.hide();
+    progressModal.modal.hide();
+  });
+}
 
 const topOpeningsModal = {
   modal: new Modal(document.getElementById('topOpeningsModal')),
   form: document.querySelector('#topOpeningsModal form'),
   mount: (res) => {
     const winRateForWhiteChart = document.getElementById('winRateForWhiteChart');
-    new Chart(winRateForWhiteChart, {
+    const chart = new Chart(winRateForWhiteChart, {
       type: 'bar',
       data: {
         labels: res.winRateForWhite.map(value => value.ECO),
         datasets: [{
-          label: 'Win Rate for White',
+          label: '1-0',
           data: res.winRateForWhite.map(value => value.total),
           backgroundColor: '#f5f5f5',
           borderColor: '#404040',
@@ -21,6 +60,11 @@ const topOpeningsModal = {
         }]
       },
       options: {
+        animation: false,
+        onHover: function(event, el) {
+          event.native.target.style.cursor = el[0] ? 'pointer' : 'default';
+        },
+        onClick: handleBarClick,
         scales: {
           x: {
             grid: {
@@ -36,18 +80,24 @@ const topOpeningsModal = {
         }
       }
     });
+
     const drawRateChart = document.getElementById('drawRateChart');
     new Chart(drawRateChart, {
       type: 'bar',
       data: {
         labels: res.drawRate.map(value => value.ECO),
         datasets: [{
-          label: 'Draw Rate',
+          label: '1/2-1/2',
           data: res.drawRate.map(value => value.total),
           backgroundColor: '#888888'
         }]
       },
       options: {
+        animation: false,
+        onHover: function(event, el) {
+          event.native.target.style.cursor = el[0] ? 'pointer' : 'default';
+        },
+        onClick: handleBarClick,
         scales: {
           x: {
             grid: {
@@ -63,13 +113,14 @@ const topOpeningsModal = {
         }
       }
     });
+
     const winRateForBlackChart = document.getElementById('winRateForBlackChart');
     new Chart(winRateForBlackChart, {
       type: 'bar',
       data: {
         labels: res.winRateForBlack.map(value => value.ECO),
         datasets: [{
-          label: 'Win Rate for Black',
+          label: '0-1',
           data: res.winRateForBlack.map(value => value.total),
           backgroundColor: '#404040',
           borderColor: '#202020',
@@ -77,6 +128,11 @@ const topOpeningsModal = {
         }]
       },
       options: {
+        animation: false,
+        onHover: function(event, el) {
+          event.native.target.style.cursor = el[0] ? 'pointer' : 'default';
+        },
+        onClick: handleBarClick,
         scales: {
           x: {
             grid: {
