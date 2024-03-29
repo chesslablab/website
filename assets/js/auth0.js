c1c17ab372b7ea93aa35b5db2d3a240b0de6d731
programarivm <info@programarivm.com>
2024-03-29 12:58:07 +0100

    Updated auth0.js

--- assets/js/auth0.js
+++ assets/js/auth0.js
@@ -2,35 +2,26 @@ import { createAuth0Client } from '@auth0/auth0-spa-js';
 import Cookies from 'js-cookie';
 import * as env from '../env.js';
 
-const loginButton = document.getElementById("login");
-
-const logoutButton = document.getElementById("logout");
-
 if (Cookies.get(`auth0.${env.AUTH0_CLIENT_ID}.is.authenticated`)) {
+  const logoutButton = document.getElementById("logout");
   logoutButton.classList.remove('d-none');
   logoutButton.addEventListener('click', (e) => {
     e.preventDefault();
     createAuth0Client({
       domain: env.AUTH0_DOMAIN,
-      clientId: env.AUTH0_CLIENT_ID,
-      authorizationParams: {
-        redirect_uri: window.location.origin
-      }
+      clientId: env.AUTH0_CLIENT_ID
     }).then(async (auth0Client) => {
       auth0Client.logout();
     });
   });
-
 } else {
+  const loginButton = document.getElementById("login");
   loginButton.classList.remove('d-none');
   loginButton.addEventListener('click', (e) => {
     e.preventDefault();
     createAuth0Client({
       domain: env.AUTH0_DOMAIN,
-      clientId: env.AUTH0_CLIENT_ID,
-      authorizationParams: {
-        redirect_uri: window.location.origin
-      }
+      clientId: env.AUTH0_CLIENT_ID
     }).then(async (auth0Client) => {
       await auth0Client.loginWithPopup();
       window.location.href = window.location.origin;
