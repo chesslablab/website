1affab4f13c3b8ab7904e3f3eadb8ea2f4ba3e1e
programarivm <info@programarivm.com>
2024-03-30 22:42:51 +0100

    Renamed ul as myChesslaBlabDropdown

--- assets/js/my_chesslablab/myChesslaBlabDropdown.js
+++ assets/js/my_chesslablab/myChesslaBlabDropdown.js
@@ -6,25 +6,25 @@ const auth0Client = await createAuth0Client({
   clientId: env.AUTH0_CLIENT_ID
 });
 
-const ul = document.querySelector('#myChesslaBlabDropdown ul');
+const myChesslaBlabDropdown = document.querySelector('#myChesslaBlabDropdown ul');
 
 if (await auth0Client.isAuthenticated()) {
   const user = await auth0Client.getUser();
   if (user.hasOwnProperty('email_verified')) {
     if (user.email_verified === true) {
-      ul.children.item(1).classList.remove('d-none');
-      ul.children.item(2).classList.remove('d-none');
+      myChesslaBlabDropdown.children.item(1).classList.remove('d-none');
+      myChesslaBlabDropdown.children.item(2).classList.remove('d-none');
     } else {
-      ul.children.item(0).classList.remove('d-none');
+      myChesslaBlabDropdown.children.item(0).classList.remove('d-none');
     }
   } else {
-    ul.children.item(0).classList.remove('d-none');
+    myChesslaBlabDropdown.children.item(0).classList.remove('d-none');
   }
 } else {
-  ul.children.item(0).classList.remove('d-none');
+  myChesslaBlabDropdown.children.item(0).classList.remove('d-none');
 }
 
-ul.children.item(0).addEventListener('click',  async (event) => {
+myChesslaBlabDropdown.children.item(0).addEventListener('click',  async (event) => {
   event.preventDefault();
   await auth0Client.loginWithPopup();
   const user = await auth0Client.getUser();
@@ -35,12 +35,12 @@ ul.children.item(0).addEventListener('click',  async (event) => {
   }
 });
 
-ul.children.item(1).addEventListener('click', (event) => {
+myChesslaBlabDropdown.children.item(1).addEventListener('click', (event) => {
   event.preventDefault();
   console.log('TODO');
 });
 
-ul.children.item(2).addEventListener('click', (event) => {
+myChesslaBlabDropdown.children.item(2).addEventListener('click', (event) => {
   event.preventDefault();
   auth0Client.logout();
 });
