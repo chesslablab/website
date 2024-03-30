acdb6ad4bcec7b9caeb87ad03d2c83150fdd42f3
programarivm <info@programarivm.com>
2024-03-30 15:28:01 +0100

    Updated auth0.js

--- assets/js/auth0.js
+++ assets/js/auth0.js
@@ -2,34 +2,16 @@ import { createAuth0Client } from '@auth0/auth0-spa-js';
 import Cookies from 'js-cookie';
 import * as env from '../env.js';
 
+const loginButton = document.getElementById('login');
+
+const logoutButton = document.getElementById('logout');
+
 const auth0Client = await createAuth0Client({
   domain: env.AUTH0_DOMAIN,
   clientId: env.AUTH0_CLIENT_ID
 });
 
-const loginButton = document.getElementById('login');
-
-loginButton.addEventListener('click', async (event) => {
-  event.preventDefault();
-  await auth0Client.loginWithPopup();
-  const user = await auth0Client.getUser();
-  if (user.email_verified === false) {
-    alert("Please verify your email. You're almost there! Just click on the link on the verification email we sent you to complete your signup.");
-  } else {
-    window.location.href = window.location.origin;
-  }
-});
-
-const logoutButton = document.getElementById('logout');
-
-logoutButton.addEventListener('click', (event) => {
-  event.preventDefault();
-  auth0Client.logout();
-});
-
-const isAuthenticated = await auth0Client.isAuthenticated();
-
-if (isAuthenticated) {
+if (await auth0Client.isAuthenticated()) {
   const user = await auth0Client.getUser();
   if (user.hasOwnProperty('email_verified')) {
     if (user.email_verified === true) {
@@ -43,3 +25,19 @@ if (isAuthenticated) {
 } else {
   loginButton.classList.remove('d-none');
 }
+
+loginButton.addEventListener('click', async (event) => {
+  event.preventDefault();
+  await auth0Client.loginWithPopup();
+  const user = await auth0Client.getUser();
+  if (user.email_verified === false) {
+    alert("Please verify your email. You're almost there! Just click on the link on the verification email we sent you to complete your signup.");
+  } else {
+    window.location.href = window.location.origin;
+  }
+});
+
+logoutButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  auth0Client.logout();
+});
