745d9d795c43c847cc0cd57df9ba7ea3ad771b73
programarivm <info@programarivm.com>
2024-03-30 15:00:22 +0100

    Updated auth0.js

--- assets/js/auth0.js
+++ assets/js/auth0.js
@@ -2,29 +2,47 @@ import { createAuth0Client } from '@auth0/auth0-spa-js';
 import Cookies from 'js-cookie';
 import * as env from '../env.js';
 
-if (Cookies.get(`auth0.${env.AUTH0_CLIENT_ID}.is.authenticated`)) {
-  const logoutButton = document.getElementById("logout");
-  logoutButton.classList.remove('d-none');
-  logoutButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    createAuth0Client({
-      domain: env.AUTH0_DOMAIN,
-      clientId: env.AUTH0_CLIENT_ID
-    }).then(async (auth0Client) => {
-      auth0Client.logout();
+createAuth0Client({
+  domain: env.AUTH0_DOMAIN,
+  clientId: env.AUTH0_CLIENT_ID
+}).then(async (auth0Client) => {
+  const loginButton = document.getElementById('login');
+
+  loginButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    auth0Client.loginWithPopup().then(() => {
+      auth0Client.getUser().then((user) => {
+        if (user.email_verified === false) {
+          alert("Please verify your email. You're almost there! Just click on the link on the verification email we sent you to complete your signup.");
+        } else {
+          window.location.href = window.location.origin;
+        }
+      });
     });
   });
-} else {
-  const loginButton = document.getElementById("login");
-  loginButton.classList.remove('d-none');
-  loginButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    createAuth0Client({
-      domain: env.AUTH0_DOMAIN,
-      clientId: env.AUTH0_CLIENT_ID
-    }).then(async (auth0Client) => {
-      await auth0Client.loginWithPopup();
-      window.location.href = window.location.origin;
-    });
+
+  const logoutButton = document.getElementById('logout');
+
+  logoutButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    auth0Client.logout();
   });
-}
+
+  const isAuthenticated = await auth0Client.isAuthenticated();
+
+  const user = await auth0Client.getUser();
+
+  if (isAuthenticated) {
+    if (user.hasOwnProperty('email_verified')) {
+      if (user.email_verified === true) {
+        logoutButton.classList.remove('d-none');
+      } else {
+        loginButton.classList.remove('d-none');
+      }
+    } else {
+      logoutButton.classList.remove('d-none');
+    }
+  } else {
+    loginButton.classList.remove('d-none');
+  }
+});
