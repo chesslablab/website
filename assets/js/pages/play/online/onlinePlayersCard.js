f608551c86aeaadf303ec37df3d4496a89fcdd20
programarivm <info@programarivm.com>
2024-03-31 23:57:44 +0200

    Updated onlinePlayersCard

--- assets/js/pages/play/online/onlinePlayersCard.js
+++ assets/js/pages/play/online/onlinePlayersCard.js
@@ -11,22 +11,14 @@ const onlinePlayersCard = {
     if (games.length > 0) {
       games.forEach(game => {
         const tr = document.createElement('tr');
-        const usernameTd = document.createElement('td');
-        const usernameText = document.createTextNode('Guest');
         const minTd = document.createElement('td');
-        const minText = document.createTextNode(game.min);
         const incrementTd = document.createElement('td');
-        const incrementText = document.createTextNode(game.increment);
         const colorTd = document.createElement('td');
-        const colorText = document.createTextNode(game.color);
         const variantTd = document.createElement('td');
-        const variantText = document.createTextNode(game.variant);
-        usernameTd.appendChild(usernameText);
-        minTd.appendChild(minText);
-        incrementTd.appendChild(incrementText);
-        colorTd.appendChild(colorText);
-        variantTd.appendChild(variantText);
-        tr.appendChild(usernameTd);
+        minTd.appendChild(document.createTextNode(game.min));
+        incrementTd.appendChild(document.createTextNode(game.increment));
+        colorTd.appendChild(document.createTextNode(game.color));
+        variantTd.appendChild(document.createTextNode(game.variant));
         tr.appendChild(minTd);
         tr.appendChild(incrementTd);
         tr.appendChild(colorTd);
@@ -36,7 +28,6 @@ const onlinePlayersCard = {
             ws.send(`/accept ${game.hash}`);
           });
         } else {
-          usernameTd.style.cursor = 'default';
           minTd.style.cursor = 'default';
           incrementTd.style.cursor = 'default';
           colorTd.style.cursor = 'default';
