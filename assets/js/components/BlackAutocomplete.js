09ba5ae4e4e7a40385fd673695d2933bc03a94e4
programarivm <info@programarivm.com>
2024-04-03 19:41:10 +0200

    Implemented BlackAutocomplete

---
+++ assets/js/components/BlackAutocomplete.js
@@ -0,0 +1,45 @@
+import AbstractComponent from './AbstractComponent.js';
+import * as env from '../../env.js';
+
+class BlackAutocomplete extends AbstractComponent {
+  mount() {
+    this.el.addEventListener('keyup', (event) => {
+      event.preventDefault();
+      if (event.target.value.length % 3 === 0) {
+        this.props.submitButton.classList.add('d-none');
+        this.props.loadingButton.classList.remove('d-none');
+        fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/autocomplete/player`, {
+          method: 'POST',
+          headers: {
+            'X-Api-Key': `${env.API_KEY}`
+          },
+          body: JSON.stringify({
+            Black: event.target.value
+          })
+        })
+        .then(res => res.json())
+        .then(res => {
+          this.props.datalist.replaceChildren();
+          res.forEach(item => {
+            const option = document.createElement('option');
+            option.appendChild(document.createTextNode(item));
+            option.addEventListener('click', (event) => {
+              event.preventDefault();
+              this.el.value = item;
+            });
+            this.props.datalist.append(option);
+          });
+        })
+        .catch(error => {
+          // TODO
+        })
+        .finally(() => {
+          this.props.submitButton.classList.remove('d-none');
+          this.props.loadingButton.classList.add('d-none');
+        });
+      }
+    });
+  }
+}
+
+export default BlackAutocomplete;
--- assets/js/pages/blackAutocomplete.js
+++ assets/js/pages/blackAutocomplete.js
@@ -1,47 +1,12 @@
-import * as env from '../../env.js';
+import BlackAutocomplete from '../components/BlackAutocomplete.js';
 
-const blackAutocomplete = {
-  input: document.querySelector('input[list="blackAutocompleteList"]'),
-  datalist: document.getElementById('blackAutocompleteList')
-}
-
-blackAutocomplete.input.addEventListener('keyup', (event) => {
-  event.preventDefault();
-  const submitButton = document.querySelector('button.autocomplete[type="submit"]');
-  const loadingButton = document.querySelector('button.autocomplete[type="button"]');
-  if (event.target.value.length % 3 === 0) {
-    submitButton.classList.add('d-none');
-    loadingButton.classList.remove('d-none');
-    fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/autocomplete/player`, {
-      method: 'POST',
-      headers: {
-        'X-Api-Key': `${env.API_KEY}`
-      },
-      body: JSON.stringify({
-        Black: event.target.value
-      })
-    })
-    .then(res => res.json())
-    .then(res => {
-      blackAutocomplete.datalist.replaceChildren();
-      res.forEach(item => {
-        const option = document.createElement('option');
-        option.appendChild(document.createTextNode(item));
-        option.addEventListener('click', (event) => {
-          event.preventDefault();
-          blackAutocomplete.input.value = item;
-        });
-        blackAutocomplete.datalist.append(option);
-      });
-    })
-    .catch(error => {
-      // TODO
-    })
-    .finally(() => {
-      submitButton.classList.remove('d-none');
-      loadingButton.classList.add('d-none');
-    });
+const blackAutocomplete = new BlackAutocomplete(
+  document.querySelector('input[list="blackAutocompleteList"]'),
+  {
+    datalist: document.getElementById('blackAutocompleteList'),
+    submitButton: document.querySelector('button.autocomplete[type="submit"]'),
+    loadingButton: document.querySelector('button.autocomplete[type="button"]')
   }
-});
+);
 
-export default blackAutocomplete;
+export default BlackAutocomplete;
