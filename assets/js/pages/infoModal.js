36f43efca4365c7928e370684b67c6fdb717bdcb
programarivm <info@programarivm.com>
2024-04-03 16:11:51 +0200

    Refactored infoModal

--- assets/js/pages/infoModal.js
+++ assets/js/pages/infoModal.js
@@ -2,17 +2,11 @@ import Modal from 'bootstrap/js/dist/modal.js';
 
 const infoModal = {
   modal: new Modal(document.getElementById('infoModal')),
-  form: document.querySelector('#infoModal form'),
   msg: (txt) => {
     const div = document.querySelector('#infoModal div.message');
     div.replaceChildren();
     div.appendChild(document.createTextNode(txt));
   }
 }
-
-infoModal.form.addEventListener('submit', event => {
-  event.preventDefault();
-  infoModal.modal.hide();
-});
 
 export default infoModal;
