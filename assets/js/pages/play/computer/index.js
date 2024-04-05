ada3ec6dd2f8f6ff567d89287fa1af952f4af1ce
programarivm <info@programarivm.com>
2024-04-05 18:17:13 +0200

    Play Computer

--- assets/js/pages/play/computer/index.js
+++ assets/js/pages/play/computer/index.js
@@ -1,6 +1,6 @@
+import boardActionsDropdown from '../../../elements/boardActionsDropdown.js';
+import historyButtons from '../../../elements/historyButtons.js';
 import playComputerModal from '../../../elements/playComputerModal.js';
-import boardActionsDropdown from '../../../pages/boardActionsDropdown.js';
-import historyButtons from '../../../pages/historyButtons.js';
 import ws from '../../../stockfishWs.js';
 
 await ws.connect();
