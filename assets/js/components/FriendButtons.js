5f181dce57e32def9c358d5250584e1423d03056
programarivm <info@programarivm.com>
2024-04-05 16:06:12 +0200

    Refactored FriendButtons

--- assets/js/components/FriendButtons.js
+++ assets/js/components/FriendButtons.js
@@ -1,17 +1,15 @@
 import AbstractComponent from './AbstractComponent.js';
-import enterInviteCodeModal from '../pages/play/online/enterInviteCodeModal.js';
-import playFriendModal from '../pages/play/online/playFriendModal.js';
 
 class FriendButtons extends AbstractComponent {
   mount() {
     this.el.children.item(0).addEventListener('click', async (event) => {
       event.preventDefault();
-      playFriendModal.modal.show();
+      this.props.playFriendModal.modal.show();
     });
 
     this.el.children.item(1).addEventListener('click', async (event) => {
       event.preventDefault();
-      enterInviteCodeModal.props.modal.show();
+      this.props.enterInviteCodeModal.props.modal.show();
     });
   }
 }
--- assets/js/pages/play/online/friendButtons.js
+++ assets/js/pages/play/online/friendButtons.js
@@ -1,5 +1,13 @@
 import FriendButtons from '../../../components/FriendButtons.js';
+import enterInviteCodeModal from './enterInviteCodeModal.js';
+import playFriendModal from './playFriendModal.js';
 
-const friendButtons = new FriendButtons(document.getElementById('friendButtons'));
+const friendButtons = new FriendButtons(
+  document.getElementById('friendButtons'),
+  {
+    enterInviteCodeModal: enterInviteCodeModal,
+    playFriendModal: playFriendModal
+  }
+);
 
 export default friendButtons;
