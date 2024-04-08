26d313e095794520322270303b14f049628131d4
programarivm <info@programarivm.com>
2024-04-08 12:34:34 +0200

    Renamed chessboardFenStringModal as fenStringModal

--- assets/js/pages/learn/fen_string/fenStringModal.js
+++ assets/js/pages/learn/fen_string/fenStringModal.js
@@ -2,18 +2,18 @@ import Modal from 'bootstrap/js/dist/modal.js';
 import { fenWebSocket } from '../../../FenWebSocket.js';
 import * as mode from '../../../../mode.js';
 
-const chessboardFenStringModal = {
-  modal: new Modal(document.getElementById('chessboardFenStringModal')),
-  form: document.querySelector('#chessboardFenStringModal form')
+const fenStringModal = {
+  modal: new Modal(document.getElementById('fenStringModal')),
+  form: document.querySelector('#fenStringModal form')
 }
 
-chessboardFenStringModal.form.addEventListener('submit', event => {
+fenStringModal.form.addEventListener('submit', event => {
   event.preventDefault();
   const add = {
     fen: event.target.elements[1].value
   };
   fenWebSocket.send(`/start ${event.target.elements[0].value} ${mode.FEN} "${JSON.stringify(add).replace(/"/g, '\\"')}"`);
-  chessboardFenStringModal.modal.hide();
+  fenStringModal.modal.hide();
 });
 
-export default chessboardFenStringModal;
+export default fenStringModal;
