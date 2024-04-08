7d034acb30906331c0b2bb690bc5c51c3c1709cc
programarivm <info@programarivm.com>
2024-04-08 19:01:22 +0200

    Refactored RavMovetextModal

--- assets/js/pages/learn/rav_movetext/index.js
+++ assets/js/pages/learn/rav_movetext/index.js
@@ -1,10 +1,10 @@
-import ravMovetextModal from './ravMovetextModal.js';
 import boardActionsDropdown from './boardActionsDropdown.js';
 import historyButtons from './historyButtons.js';
 import ravMovesTable from './ravMovesTable.js';
+import { ravMovetextModal } from './ravMovetextModal.js';
 import chessboard from '../../chessboard.js';
 import { progressModal } from '../../ProgressModal.js';
 
 localStorage.clear();
 
-ravMovetextModal.modal.show();
+ravMovetextModal.props.modal.show();
--- assets/js/pages/learn/rav_movetext/ravMovetextModal.js
+++ assets/js/pages/learn/rav_movetext/ravMovetextModal.js
@@ -1,57 +1,63 @@
 import Modal from 'bootstrap/js/dist/modal.js';
 import ravMovesTable from './ravMovesTable.js';
+import AbstractComponent from '../../../AbstractComponent.js';
 import { progressModal } from '../../ProgressModal.js';
 import * as env from '../../../../env.js';
 import * as mode from '../../../../mode.js';
 import * as variant from '../../../../variant.js';
 
-const ravMovetextModal = {
-  modal: new Modal(document.getElementById('ravMovetextModal')),
-  form: document.querySelector('#ravMovetextModal form')
+export class RavMovetextModal extends AbstractComponent {
+  mount() {
+    this.props.form.getElementsByTagName('select')[0].addEventListener('change', event => {
+      event.preventDefault();
+      if (event.target.value === variant.CHESS_960) {
+        this.props.form.getElementsByClassName('startPos')[0].classList.remove('d-none');
+      } else {
+        this.props.form.getElementsByClassName('startPos')[0].classList.add('d-none');
+      }
+    });
+
+    this.props.form.addEventListener('submit', event => {
+      event.preventDefault();
+      progressModal.props.modal.show();
+      const formData = new FormData(this.props.form);
+      fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/play/rav`, {
+        method: 'POST',
+        headers: {
+          'X-Api-Key': `${env.API_KEY}`
+        },
+        body: JSON.stringify({
+          variant: formData.get('variant'),
+          movetext: formData.get('rav'),
+        })
+      })
+      .then(res => res.json())
+      .then(res => {
+        ravMovesTable.current = res.fen.length - 1;
+        ravMovesTable.props.chessboard.setPosition(res.fen[res.fen.length - 1]);
+        ravMovesTable.props = {
+          ...ravMovesTable.props,
+          filtered: res.filtered,
+          breakdown: res.breakdown,
+          fen: res.fen
+        };
+        ravMovesTable.mount();
+      })
+      .catch(error => {
+        // TODO
+      })
+      .finally(() => {
+        this.props.modal.hide();
+        progressModal.props.modal.hide();
+      });
+    });
+  }
 }
 
-ravMovetextModal.form.getElementsByTagName('select')[0].addEventListener('change', event => {
-  event.preventDefault();
-  if (event.target.value === variant.CHESS_960) {
-    ravMovetextModal.form.getElementsByClassName('startPos')[0].classList.remove('d-none');
-  } else {
-    ravMovetextModal.form.getElementsByClassName('startPos')[0].classList.add('d-none');
+export const ravMovetextModal = new RavMovetextModal(
+  document.getElementById('ravMovetextModal'),
+  {
+    modal: new Modal(document.getElementById('ravMovetextModal')),
+    form: document.querySelector('#ravMovetextModal form')
   }
-});
-
-ravMovetextModal.form.addEventListener('submit', event => {
-  event.preventDefault();
-  progressModal.props.modal.show();
-  const formData = new FormData(ravMovetextModal.form);
-  fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/play/rav`, {
-    method: 'POST',
-    headers: {
-      'X-Api-Key': `${env.API_KEY}`
-    },
-    body: JSON.stringify({
-      variant: formData.get('variant'),
-      movetext: formData.get('rav'),
-    })
-  })
-  .then(res => res.json())
-  .then(res => {
-    ravMovesTable.current = res.fen.length - 1;
-    ravMovesTable.props.chessboard.setPosition(res.fen[res.fen.length - 1]);
-    ravMovesTable.props = {
-      ...ravMovesTable.props,
-      filtered: res.filtered,
-      breakdown: res.breakdown,
-      fen: res.fen
-    };
-    ravMovesTable.mount();
-  })
-  .catch(error => {
-    // TODO
-  })
-  .finally(() => {
-    ravMovetextModal.modal.hide();
-    progressModal.props.modal.hide();
-  });
-});
-
-export default ravMovetextModal;
+);
