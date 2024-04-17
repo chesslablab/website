ffab68e975a000d9a6d3e9a967533df739b49655
programarivm <info@programarivm.com>
2024-04-17 10:53:12 +0200

    Updated InfoModal

--- assets/js/pages/InfoModal.js
+++ assets/js/pages/InfoModal.js
@@ -1,11 +1,12 @@
 import Modal from 'bootstrap/js/dist/modal.js';
 import AbstractComponent from '../AbstractComponent.js';
+import { trans } from '../i18n.js';
 
 export class InfoModal extends AbstractComponent {
   mount() {
     const div = this.el.querySelector('div.message');
     div.replaceChildren();
-    div.appendChild(document.createTextNode(this.props.msg));
+    div.appendChild(document.createTextNode(`${trans(this.props.msg)}.`));
   }
 }
 
