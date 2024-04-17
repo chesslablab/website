e9b2c2f54d84cba2b8e8f4e7c0417ada5870b158
programarivm <info@programarivm.com>
2024-04-17 11:13:02 +0200

    Implemented translations

---
+++ assets/js/translations/messages.es.js
@@ -0,0 +1,5 @@
+const messagesEs = {
+  "Waiting for player to join": "Esperando a que el jugador o jugadora se una"
+};
+
+export default messagesEs;
---
+++ assets/js/translations/messages.fr.js
@@ -0,0 +1,5 @@
+const messagesFr = {
+  "Waiting for player to join": "En attente que le joueur rejoigne"
+};
+
+export default messagesFr;
