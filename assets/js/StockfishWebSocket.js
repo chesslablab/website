732e75e847e38cd632c5c3a985f2c62872b3e2ba
programarivm <info@programarivm.com>
2024-05-06 16:26:55 +0200

    Updated to PHP Chess 1.4.25

--- assets/js/StockfishWebSocket.js
+++ assets/js/StockfishWebSocket.js
@@ -67,8 +67,8 @@ export class StockfishWebSocket {
             break;
 
           case '/legal' === msg:
-            Object.keys(data['/legal'].fen).forEach(key => {
-              chessboard.addMarker(MARKER_TYPE.dot, key);
+            data['/legal'].forEach(sq => {
+              chessboard.addMarker(MARKER_TYPE.dot, sq);
             });
             break;
 
