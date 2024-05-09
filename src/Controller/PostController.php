d5bc9f109d549df59c2e5f683ed00b6c3ba5f06b
programarivm <info@programarivm.com>
2024-05-09 16:22:55 +0200

    Added the post metadata

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,19 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Yaml\Yaml;
 
 class PostController extends AbstractController
 {
     const DATA_FOLDER = __DIR__ . '/../../posts';
 
-    public function hello_world(): Response
+    public function hello_world(Request $request): Response
     {
+        $routes = Yaml::parseFile("../config/routes.yaml");
+        $metadata = $routes[$request->attributes->get('_route')]['options']['metadata'];
         $content = file_get_contents(self::DATA_FOLDER . '/hello-world.md');
 
         return $this->render('post.html.twig', [
-            'title' => 'Hello World!',
-            'description' => 'This is my first post.',
+            'title' => $metadata['title'],
+            'description' => $metadata['description'],
             'content' => $content,
         ]);
     }
