7019e860321e61c36fdfb372801d5f6fe51d95b9
programarivm <info@programarivm.com>
2024-05-13 19:02:32 +0200

    Implemented EndgameModal

--- assets/js/pages/training/endgame/index.js
+++ assets/js/pages/training/endgame/index.js
@@ -1,1 +1,8 @@
-console.log('TODO');
+import { endgameModal } from './EndgameModal.js';
+import { stockfishWebSocket } from '../../../StockfishWebSocket.js';
+
+await stockfishWebSocket.connect();
+
+sessionStorage.clear();
+
+endgameModal.props.modal.show();
