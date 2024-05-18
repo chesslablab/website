62cb2e03cb7708adafd190254bab53025fc81626
Aliberk Sandıkçı <git@aliberksandikci.com.tr>
2024-05-18 15:35:55 +0300

    Update i18n.js

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -1,11 +1,14 @@
 import messagesEs from './translations/messages.es.js';
 import messagesFr from './translations/messages.fr.js';
+import messagesTr from './translations/messages.tr.js';
 
 export const trans = (string) => {
   if (document.documentElement.lang === 'es') {
     return messagesEs[string];
   } else if (document.documentElement.lang === 'fr') {
     return messagesFr[string];
+  } else if (document.documentElement.lang === 'tr') {
+    return messagesTr[string];
   }
 
   return string;
