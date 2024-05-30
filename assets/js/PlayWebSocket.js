5a29b333e5df9522c79ad92b42e97a64c3eee80f
programarivm <info@programarivm.com>
2024-05-30 15:17:42 +0200

    Updated PlayWebSocket

--- assets/js/PlayWebSocket.js
+++ assets/js/PlayWebSocket.js
@@ -1,9 +1,7 @@
-import { COLOR, FEN, INPUT_EVENT_TYPE, MARKER_TYPE } from '@chesslablab/cmblab';
+import { COLOR, FEN, MARKER_TYPE } from '@chesslablab/cmblab';
 import { Movetext } from '@chesslablab/jsblab';
 import { jwtDecode } from 'jwt-decode';
-import chessboard from './pages/chessboard.js';
-import { infoModal } from './pages/InfoModal.js';
-import { progressModal } from './pages/ProgressModal.js';
+import AbstractWebSocket from './AbstractWebSocket.js';
 import { copyInviteCodeModal } from './pages/play/online/CopyInviteCodeModal.js';
 import { createGameModal } from './pages/play/online/CreateGameModal.js';
 import { drawModal } from './pages/play/online/DrawModal.js';
@@ -14,47 +12,29 @@ import { rematchModal } from './pages/play/online/RematchModal.js';
 import { takebackModal } from './pages/play/online/TakebackModal.js';
 import * as action from '../action.js';
 import * as env from '../env.js';
-import * as mode from '../mode.js';
 
-export default class PlayWebSocket {
+export default class PlayWebSocket extends AbstractWebSocket {
   _timerTableInterval;
 
   constructor() {
-    chessboard.enableMoveInput((event) => {
-      if (event.type === INPUT_EVENT_TYPE.movingOverSquare) {
-        return;
-      }
-
-      if (event.type !== INPUT_EVENT_TYPE.moveInputFinished) {
-        event.chessboard.removeMarkers(MARKER_TYPE.dot);
-        event.chessboard.removeMarkers(MARKER_TYPE.bevel);
-      }
-
-      if (event.type === INPUT_EVENT_TYPE.moveInputStarted) {
-        this.send(`/legal ${event.square}`);
-        return true;
-      } else if (event.type === INPUT_EVENT_TYPE.validateMoveInput) {
-        this.send(`/play_lan ${event.piece.charAt(0)} ${event.squareFrom}${event.squareTo}`);
-        return true;
-      }
-    });
+    super();
 
     playPanel.props.gameActionsDropdown.props.ul.children.item(0).addEventListener('click', (event) => {
       event.preventDefault();
       sessionStorage.setItem('takeback', action.PROPOSE);
       this.send('/takeback propose');
-      infoModal.props.msg = "Waiting for your opponent to accept or decline";
-      infoModal.mount();
-      infoModal.props.modal.show();
+      this._infoModal.props.msg = "Waiting for your opponent to accept or decline";
+      this._infoModal.mount();
+      this._infoModal.props.modal.show();
     });
 
     playPanel.props.gameActionsDropdown.props.ul.children.item(1).addEventListener('click', (event) => {
       event.preventDefault();
       sessionStorage.setItem('draw', action.PROPOSE);
       this.send('/draw propose');
-      infoModal.props.msg = "Waiting for your opponent to accept or decline";
-      infoModal.mount();
-      infoModal.props.modal.show();
+      this._infoModal.props.msg = "Waiting for your opponent to accept or decline";
+      this._infoModal.mount();
+      this._infoModal.props.modal.show();
     });
 
     playPanel.props.gameActionsDropdown.props.ul.children.item(2).addEventListener('click', (event) => {
@@ -66,33 +46,31 @@ export default class PlayWebSocket {
       event.preventDefault();
       sessionStorage.setItem('rematch', action.PROPOSE);
       this.send('/rematch propose');
-      infoModal.props.msg = "Waiting for your opponent to accept or decline";
-      infoModal.mount();
-      infoModal.props.modal.show();
+      this._infoModal.props.msg = "Waiting for your opponent to accept or decline";
+      this._infoModal.mount();
+      this._infoModal.props.modal.show();
     });
 
     playPanel.props.finishedButtons.el.children.item(1).addEventListener('click', (event) => {
       event.preventDefault();
-      chessboard.setPosition(FEN.start, true);
+      this._chessboard.setPosition(FEN.start, true);
       playOnlineButtons.el.classList.remove('d-none');
       playPanel.el.classList.add('d-none');
     });
-
-    this.socket = null;
   }
 
   connect() {
-    progressModal.props.modal.show();
+    this._progressModal.props.modal.show();
 
     return new Promise((resolve, reject) => {
-      this.socket = new WebSocket(`${env.WEBSOCKET_SCHEME}://${env.WEBSOCKET_HOST}:${env.WEBSOCKET_PORT}`);
+      this._socket = new WebSocket(`${env.WEBSOCKET_SCHEME}://${env.WEBSOCKET_HOST}:${env.WEBSOCKET_PORT}`);
 
-      this.socket.onopen = () => {
-        progressModal.props.modal.hide();
+      this._socket.onopen = () => {
+        this._progressModal.props.modal.hide();
         resolve();
       };
 
-      this.socket.onmessage = (res) => {
+      this._socket.onmessage = (res) => {
         const data = JSON.parse(res.data);
         const msg = Object.keys(data)[0];
         switch (true) {
@@ -109,10 +87,10 @@ export default class PlayWebSocket {
             if (data['/start'].jwt) {
               copyInviteCodeModal.props.form.elements['hash'].value = data['/start'].hash;
               const jwtDecoded = jwtDecode(data['/start'].jwt);
-              chessboard.setPosition(data['/start'].fen, true);
-              chessboard.setOrientation(jwtDecoded.color);
-              chessboard.props.variant = data['/start'].variant;
-              chessboard.props.startPos = data['/start'].startPos;
+              this._chessboard.setPosition(data['/start'].fen, true);
+              this._chessboard.setOrientation(jwtDecoded.color);
+              this._chessboard.props.variant = data['/start'].variant;
+              this._chessboard.props.startPos = data['/start'].startPos;
               createGameModal.props.modal.hide();
               this.send('/online_games');
               sessionStorage.setItem('hash', data['/start'].hash);
@@ -123,13 +101,13 @@ export default class PlayWebSocket {
 
           case '/legal' === msg:
             data['/legal'].forEach(sq => {
-              chessboard.addMarker(MARKER_TYPE.dot, sq);
+              this._chessboard.addMarker(MARKER_TYPE.dot, sq);
             });
             break;
 
           case '/play_lan' === msg:
             if (data['/play_lan'].isValid) {
-              chessboard.setPosition(data['/play_lan'].fen, true);
+              this._chessboard.setPosition(data['/play_lan'].fen, true);
               playPanel.props.sanMovesBrowser.current = playPanel.props.sanMovesBrowser.props.fen.length;
               playPanel.props.sanMovesBrowser.props.movetext = Movetext.notation(localStorage.getItem('notation'), data['/play_lan'].movetext);
               playPanel.props.sanMovesBrowser.props.fen = playPanel.props.sanMovesBrowser.props.fen.concat(data['/play_lan'].fen);
@@ -140,27 +118,17 @@ export default class PlayWebSocket {
                 w: data['/play_lan'].timer.w,
                 b: data['/play_lan'].timer.b
               };
-              if (data['/play_lan'].isMate) {
-                infoModal.props.msg = data['/play_lan'].turn === COLOR.black ? 'White wins' : 'Black wins';
-                infoModal.mount();
-                infoModal.props.modal.show();
-                this._end();
-              } else if (data['/play_lan'].isFivefoldRepetition) {
-                infoModal.props.msg = "Draw by fivefold repetition";
-                infoModal.mount();
-                infoModal.props.modal.show();
-                this._end();
-              }
+              this._gameOver(data['/play_lan']);
             } else {
-              chessboard.setPosition(data['/play_lan'].fen, false);
+              this._chessboard.setPosition(data['/play_lan'].fen, false);
             }
             break;
 
           case '/undo' === msg:
-            chessboard.setPosition(data['/undo'].fen, true);
+            this._chessboard.setPosition(data['/undo'].fen, true);
             if (!data['/undo'].movetext) {
-              chessboard.state.inputWhiteEnabled = true;
-              chessboard.state.inputBlackEnabled = false;
+              this._chessboard.state.inputWhiteEnabled = true;
+              this._chessboard.state.inputBlackEnabled = false;
             }
             playPanel.props.sanMovesBrowser.current -= 1;
             playPanel.props.sanMovesBrowser.props.fen.splice(-1);
@@ -172,20 +140,20 @@ export default class PlayWebSocket {
             if (data['/accept'].jwt) {
               const jwtDecoded = jwtDecode(data['/accept'].jwt);
               const turn = jwtDecoded.fen.split(' ')[1];
-              chessboard.setPosition(jwtDecoded.fen, true);
+              this._chessboard.setPosition(jwtDecoded.fen, true);
               if (!sessionStorage.getItem('color')) {
                 if (jwtDecoded.color === COLOR.white) {
-                  chessboard.setOrientation(COLOR.black);
+                  this._chessboard.setOrientation(COLOR.black);
                   sessionStorage.setItem('color', COLOR.black);
                 } else {
-                  chessboard.setOrientation(COLOR.white);
+                  this._chessboard.setOrientation(COLOR.white);
                   sessionStorage.setItem('color', COLOR.white);
                 }
               }
               this._toggleInput(turn);
               enterInviteCodeModal.props.modal.hide();
               createGameModal.props.modal.hide();
-              infoModal.props.modal.hide();
+              this._infoModal.props.modal.hide();
               sessionStorage.setItem('hash', data['/accept'].hash);
               playPanel.props.timerTable.props = {
                 turn: turn,
@@ -198,9 +166,9 @@ export default class PlayWebSocket {
               this.send('/online_games');
             } else {
               enterInviteCodeModal.props.modal.hide();
-              infoModal.props.msg = "Invalid invite code, please try again";
-              infoModal.mount();
-              infoModal.props.modal.show();
+              this._infoModal.props.msg = "Invalid invite code, please try again";
+              this._infoModal.mount();
+              this._infoModal.props.modal.show();
             }
             break;
 
@@ -211,13 +179,13 @@ export default class PlayWebSocket {
               }
             } else if (data['/takeback'].action === action.DECLINE) {
               takebackModal.props.modal.hide();
-              infoModal.props.msg = "Takeback declined";
-              infoModal.mount();
-              infoModal.props.modal.show();
+              this._infoModal.props.msg = "Takeback declined";
+              this._infoModal.mount();
+              this._infoModal.props.modal.show();
             } else if (data['/takeback'].action === action.ACCEPT) {
-              infoModal.props.msg = "Takeback accepted";
-              infoModal.mount();
-              infoModal.props.modal.show();
+              this._infoModal.props.msg = "Takeback accepted";
+              this._infoModal.mount();
+              this._infoModal.props.modal.show();
             }
             sessionStorage.removeItem('takeback');
             break;
@@ -229,14 +197,14 @@ export default class PlayWebSocket {
               }
             } else if (data['/draw'].action === action.DECLINE) {
               drawModal.props.modal.hide();
-              infoModal.props.msg = "Draw offer declined";
-              infoModal.mount();
-              infoModal.props.modal.show();
+              this._infoModal.props.msg = "Draw offer declined";
+              this._infoModal.mount();
+              this._infoModal.props.modal.show();
             } else if (data['/draw'].action === action.ACCEPT) {
               this._end();
-              infoModal.props.msg = "Draw offer accepted";
-              infoModal.mount();
-              infoModal.props.modal.show();
+              this._infoModal.props.msg = "Draw offer accepted";
+              this._infoModal.mount();
+              this._infoModal.props.modal.show();
             }
             sessionStorage.removeItem('draw');
             break;
@@ -244,9 +212,9 @@ export default class PlayWebSocket {
           case '/resign' === msg:
             if (data['/resign'].action === action.ACCEPT) {
               this._end();
-              infoModal.props.msg = "The game is resigned";
-              infoModal.mount();
-              infoModal.props.modal.show();
+              this._infoModal.props.msg = "The game is resigned";
+              this._infoModal.mount();
+              this._infoModal.props.modal.show();
             }
             break;
 
@@ -257,7 +225,7 @@ export default class PlayWebSocket {
               }
             } else if (data['/rematch'].action === action.DECLINE) {
               rematchModal.props.modal.hide();
-              infoModal.props.modal.hide();
+              this._infoModal.props.modal.hide();
             } else if (data['/rematch'].action === action.ACCEPT) {
               this.send(`/restart ${sessionStorage.getItem('hash')}`);
             }
@@ -266,12 +234,12 @@ export default class PlayWebSocket {
 
           case '/restart' === msg:
             if (data['/restart'].jwt) {
-              infoModal.props.modal.hide();
+              this._infoModal.props.modal.hide();
               const jwtDecoded = jwtDecode(data['/restart'].jwt);
               const turn = jwtDecoded.fen.split(' ')[1];
-              chessboard.setPosition(jwtDecoded.fen, true);
+              this._chessboard.setPosition(jwtDecoded.fen, true);
               this._toggleInput(turn);
-              chessboard.view.visualizeInputState();
+              this._chessboard.view.visualizeInputState();
               playPanel.props.sanMovesBrowser.current = 0;
               playPanel.props.sanMovesBrowser.props.fen = [
                 jwtDecoded.fen
@@ -295,9 +263,9 @@ export default class PlayWebSocket {
             if (data['/leave'].action === action.ACCEPT) {
               this._end();
               playPanel.props.finishedButtons.el.children.item(0).classList.add('d-none');
-              infoModal.props.msg = "Your opponent is gone";
-              infoModal.mount();
-              infoModal.props.modal.show();
+              this._infoModal.props.msg = "Your opponent is gone";
+              this._infoModal.mount();
+              this._infoModal.props.modal.show();
             }
             break;
 
@@ -311,43 +279,36 @@ export default class PlayWebSocket {
         }
       };
 
-      this.socket.onclose = (err) => {
+      this._socket.onclose = (err) => {
         console.log('The connection has been lost, please reload the page.');
         reject(err);
       };
 
-      this.socket.onerror = (err) => {
+      this._socket.onerror = (err) => {
         console.log('The connection has been lost, please reload the page.');
         reject(err);
       };
     });
   }
 
-  send(msg) {
-    if (this.socket) {
-      this.socket.send(msg);
-    }
-  }
-
   _end() {
+    super._end();
     playPanel.props.gameActionsDropdown.el.classList.add('d-none');
     playPanel.props.finishedButtons.el.classList.remove('d-none');
-    chessboard.state.inputWhiteEnabled = false;
-    chessboard.state.inputBlackEnabled = false;
     clearInterval(this._timerTableInterval);
   }
 
   _toggleInput(turn) {
-    chessboard.state.inputWhiteEnabled = false;
-    chessboard.state.inputBlackEnabled = false;
+    this._chessboard.state.inputWhiteEnabled = false;
+    this._chessboard.state.inputBlackEnabled = false;
     if (turn === sessionStorage.getItem('color')) {
       if (turn === COLOR.white) {
-        chessboard.state.inputWhiteEnabled = true;
+        this._chessboard.state.inputWhiteEnabled = true;
       } else {
-        chessboard.state.inputBlackEnabled = true;
+        this._chessboard.state.inputBlackEnabled = true;
       }
     }
-    chessboard.view.visualizeInputState();
+    this._chessboard.view.visualizeInputState();
   }
 }
 
