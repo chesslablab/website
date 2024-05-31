3de2041f7ece990869f06803a2096a9f44ba184a
programarivm <info@programarivm.com>
2024-05-31 16:02:29 +0200

    Updated EventStatsModal

--- assets/js/pages/database/event_stats/EventStatsModal.js
+++ assets/js/pages/database/event_stats/EventStatsModal.js
@@ -14,59 +14,56 @@ export class EventStatsModal extends AbstractComponent {
   _nBars = 25;
 
   mount() {
-    const handleBarClick = (event, clickedElements) => {
+    const handleBarClick = async (event, clickedElements) => {
       const formData = new FormData(this.props.form);
       this.props.progressModal.props.modal.show();
       if (clickedElements.length === 0) {
         return;
       }
       const { dataIndex, raw } = clickedElements[0].element.$context;
-      fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/search`, {
-        method: 'POST',
-        headers: {
-          'X-Api-Key': `${env.API_KEY}`
-        },
-        body: JSON.stringify({
-          Event: formData.get('Event'),
-          Result: formData.get('Result'),
-          ECO: event.chart.data.labels[dataIndex]
-        })
-      })
-      .then(res => res.json())
-      .then(res => {
-        this.props.movesMetadataTable.props = res[0];
+      try {
+        const res = await fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/search`, {
+          method: 'POST',
+          headers: {
+            'X-Api-Key': `${env.API_KEY}`
+          },
+          body: JSON.stringify({
+            Event: formData.get('Event'),
+            Result: formData.get('Result'),
+            ECO: event.chart.data.labels[dataIndex]
+          })
+        });
+        const data = await res.json();
+        this.props.movesMetadataTable.props = data[0];
         this.props.movesMetadataTable.mount();
         const add = {
-          movetext: res[0].movetext
+          movetext: data[0].movetext
         };
         sanWebSocket.send(`/start classical ${mode.SAN} "${JSON.stringify(add).replace(/"/g, '\\"')}"`);
-      })
-      .catch(error => {
+      } catch (error) {
         // TODO
-      })
-      .finally(() => {
-        this.props.modal.hide();
-        this.props.progressModal.props.modal.hide();
-      });
+      }
+      this.props.modal.hide();
+      this.props.progressModal.props.modal.hide();
     }
 
-    this.props.form.addEventListener('submit', event => {
+    this.props.form.addEventListener('submit', async (event) => {
       event.preventDefault();
       this.props.progressModal.props.modal.show();
       const formData = new FormData(this.props.form);
       const eventStatsChart = document.getElementById('eventStatsChart');
-      fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/stats/event`, {
-        method: 'POST',
-        headers: {
-          'X-Api-Key': `${env.API_KEY}`
-        },
-        body: JSON.stringify({
-          Event: formData.get('Event'),
-          Result: formData.get('Result')
-        })
-      })
-      .then(res => res.json())
-      .then(res => {
+      try {
+        const res = await fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/stats/event`, {
+          method: 'POST',
+          headers: {
+            'X-Api-Key': `${env.API_KEY}`
+          },
+          body: JSON.stringify({
+            Event: formData.get('Event'),
+            Result: formData.get('Result')
+          })
+        });
+        const data = await res.json();
         while (eventStatsChart.firstChild) {
           eventStatsChart.removeChild(eventStatsChart.firstChild);
         }
@@ -75,9 +72,9 @@ export class EventStatsModal extends AbstractComponent {
         const chart = new Chart(canvas, {
           type: 'bar',
           data: {
-            labels: res.map(value => value.ECO).slice(0, this._nBars),
+            labels: data.map(value => value.ECO).slice(0, this._nBars),
             datasets: [{
-              data: res.map(value => value.total).slice(0, this._nBars),
+              data: data.map(value => value.total).slice(0, this._nBars),
               backgroundColor: formData.get('Result') === '1-0'
                 ? '#c0c0c0'
                 : formData.get('Result') === '1/2-1/2'
@@ -113,13 +110,10 @@ export class EventStatsModal extends AbstractComponent {
             }
           }
         });
-      })
-      .catch(error => {
+      } catch (error) {
         // TODO
-      })
-      .finally(() => {
-        this.props.progressModal.props.modal.hide();
-      });
+      }
+      this.props.progressModal.props.modal.hide();
     });
   }
 }
