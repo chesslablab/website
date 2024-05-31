756c01a39c6ccc7b142100497eaa9eb2436a1443
programarivm <info@programarivm.com>
2024-05-31 18:49:13 +0200

    Deleted TODO

--- assets/js/pages/BlackAutocomplete.js
+++ assets/js/pages/BlackAutocomplete.js
@@ -30,7 +30,6 @@ export class BlackAutocomplete extends AbstractComponent {
             this.props.datalist.append(option);
           });
         } catch (error) {
-          // TODO
         }
         this.props.submitButton.classList.remove('d-none');
         this.props.loadingButton.classList.add('d-none');
