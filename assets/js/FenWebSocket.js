fd285b58c26b588d75e24a03399c94a641c4c63f
programarivm <info@programarivm.com>
2024-05-31 12:31:12 +0200

    Updated FenWebSocket

--- assets/js/FenWebSocket.js
+++ assets/js/FenWebSocket.js
@@ -12,27 +12,24 @@ export class FenWebSocket extends AbstractWebSocket {
     fenPanel.props.gameStudyDropdown.props.ul.children.item(3).addEventListener('click', async (event) => {
       event.preventDefault();
       this._progressModal.props.modal.show();
-      await fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/tutor/fen`, {
-        method: 'POST',
-        headers: {
-          'X-Api-Key': `${env.API_KEY}`
-        },
-        body: JSON.stringify({
-          fen: fenPanel.props.sanMovesBrowser.props.fen[fenPanel.props.sanMovesBrowser.current]
-        })
-      })
-      .then(res => res.json())
-      .then(res => {
-        fenPanel.props.explainPositionModal.props.explanation = res;
+      try {
+        const res = await fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/tutor/fen`, {
+          method: 'POST',
+          headers: {
+            'X-Api-Key': `${env.API_KEY}`
+          },
+          body: JSON.stringify({
+            fen: fenPanel.props.sanMovesBrowser.props.fen[fenPanel.props.sanMovesBrowser.current]
+          })
+        });
+        fenPanel.props.explainPositionModal.props.explanation = await res.json();
         fenPanel.props.explainPositionModal.mount();
         fenPanel.props.explainPositionModal.props.modal.show();
-      })
-      .catch(error => {
+      } catch (error) {
         // TODO
-      })
-      .finally(() => {
+      } finally {
         this._progressModal.props.modal.hide();
-      });
+      }
     });
 
     fenPanel.props.gameActionsDropdown.props.ul.children.item(0).addEventListener('click', (event) => {
