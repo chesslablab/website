dcd1bffe91b24cf9b7ece77b4b49a03ba6928ae9
programarivm <info@programarivm.com>
2024-05-31 13:24:53 +0200

    Updted EventAutocomplete

--- assets/js/pages/EventAutocomplete.js
+++ assets/js/pages/EventAutocomplete.js
@@ -3,24 +3,24 @@ import * as env from '../../env.js';
 
 export class EventAutocomplete extends AbstractComponent {
   mount() {
-    this.el.addEventListener('keyup', (event) => {
+    this.el.addEventListener('keyup', async (event) => {
       event.preventDefault();
       if (event.target.value.length % 3 === 0) {
         this.props.submitButton.classList.add('d-none');
         this.props.loadingButton.classList.remove('d-none');
-        fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/autocomplete/event`, {
-          method: 'POST',
-          headers: {
-            'X-Api-Key': `${env.API_KEY}`
-          },
-          body: JSON.stringify({
-            Event: event.target.value
-          })
-        })
-        .then(res => res.json())
-        .then(res => {
-          this.props.datalist.replaceChildren();
-          res.forEach(item => {
+        this.props.datalist.replaceChildren();
+        try {
+          const res = await fetch(`${env.API_SCHEME}://${env.API_HOST}:${env.API_PORT}/${env.API_VERSION}/autocomplete/event`, {
+            method: 'POST',
+            headers: {
+              'X-Api-Key': `${env.API_KEY}`
+            },
+            body: JSON.stringify({
+              Event: event.target.value
+            })
+          });
+          const data = await res.json();
+          data.forEach(item => {
             const option = document.createElement('option');
             option.appendChild(document.createTextNode(item));
             option.addEventListener('click', (event) => {
@@ -29,14 +29,11 @@ export class EventAutocomplete extends AbstractComponent {
             });
             this.props.datalist.append(option);
           });
-        })
-        .catch(error => {
+        } catch (error) {
           // TODO
-        })
-        .finally(() => {
-          this.props.submitButton.classList.remove('d-none');
-          this.props.loadingButton.classList.add('d-none');
-        });
+        }
+        this.props.submitButton.classList.remove('d-none');
+        this.props.loadingButton.classList.add('d-none');
       }
     });
   }
