20fe16312108fda1b23ef7a2e08ecacd7053a00d
Gerard <gerarddoncel41@gmail.com>
2024-06-27 00:35:44 +0200

    translation files added

---
+++ assets/js/translations/messages.de.js
@@ -0,0 +1,19 @@
+const messagesDe = {
+  // Abstract
+  "Draw by the fifty-move rule": "Remis nach der Fünfzig-Züge-Regel",
+  // Play
+  "Waiting for player to join": "Warten auf Spielerbeitritt",
+  // PlayWebSocket
+  "Waiting for your opponent to accept or decline": "Warten auf die Annahme oder Ablehnung durch den Gegner",
+  "Invalid invite code, please try again": "Ungültiger Einladungscode, bitte erneut versuchen",
+  "Takeback declined": "Zurücknahme abgelehnt",
+  "Takeback accepted": "Zurücknahme akzeptiert",
+  "Draw offer declined": "Remisangebot abgelehnt",
+  "Draw offer accepted": "Remisangebot akzeptiert",
+  "The game is resigned": "Die Partie wurde aufgegeben",
+  "Your opponent is gone": "Dein Gegner ist verschwunden",
+  // SanWebSocket
+  "Invalid SAN movetext, please try again": "Ungültiger SAN-Zugtext, bitte erneut versuchen"
+};
+
+export default messagesDe;
