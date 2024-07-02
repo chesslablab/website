7740b0b3407b3205c7ff928f00e404acb0fc75d8
Jordi Bassagana <info@programarivm.com>
2024-07-02 17:58:57 +0200

    Update SettingsForm.js

--- assets/js/pages/settings/SettingsForm.js
+++ assets/js/pages/settings/SettingsForm.js
@@ -1,47 +1,48 @@
-import AbstractComponent from "../../AbstractComponent.js";
+import AbstractComponent from '../../AbstractComponent.js';
 
 export class SettingsForm extends AbstractComponent {
-	mount() {
-		this.el.querySelector('select[name="locale"]').value =
-			localStorage.getItem("local") || "en";
+  mount() {
+    if (localStorage.getItem('locale') !== 'en') {
+      this.el.querySelector('select[name="locale"]').value = localStorage.getItem('locale');
+    } else {
+      this.el.querySelector('select[name="locale"]').value = 'en';
+    }
 
-		if (localStorage.getItem("theme") === "dark") {
-			this.el.querySelector('select[name="theme"]').value = "dark";
-		} else {
-			this.el.querySelector('select[name="theme"]').value = "light";
-		}
+    if (localStorage.getItem('theme') === 'dark') {
+      this.el.querySelector('select[name="theme"]').value = 'dark';
+    } else {
+      this.el.querySelector('select[name="theme"]').value = 'light';
+    }
 
-		if (localStorage.getItem("format") === "table") {
-			this.el.querySelector('select[name="format"]').value = "table";
-		} else {
-			this.el.querySelector('select[name="format"]').value = "inline";
-		}
+    if (localStorage.getItem('format') === 'table') {
+      this.el.querySelector('select[name="format"]').value = 'table';
+    } else {
+      this.el.querySelector('select[name="format"]').value = 'inline';
+    }
 
-		if (localStorage.getItem("notation") === "san") {
-			this.el.querySelector('select[name="notation"]').value = "san";
-		} else {
-			this.el.querySelector('select[name="notation"]').value = "fan";
-		}
+    if (localStorage.getItem('notation') === 'san') {
+      this.el.querySelector('select[name="notation"]').value = 'san';
+    } else {
+      this.el.querySelector('select[name="notation"]').value = 'fan';
+    }
 
-		if (localStorage.getItem("set") === "staunty") {
-			this.el.querySelector('select[name="set"]').value = "staunty";
-		} else {
-			this.el.querySelector('select[name="set"]').value = "classical";
-		}
+    if (localStorage.getItem('set') === 'staunty') {
+      this.el.querySelector('select[name="set"]').value = 'staunty';
+    } else {
+      this.el.querySelector('select[name="set"]').value = 'classical';
+    }
 
-		this.el.addEventListener("submit", (event) => {
-			event.preventDefault();
-			const formData = new FormData(this.el);
-			localStorage.setItem("locale", formData.get("locale"));
-			localStorage.setItem("theme", formData.get("theme"));
-			localStorage.setItem("format", formData.get("format"));
-			localStorage.setItem("notation", formData.get("notation"));
-			localStorage.setItem("set", formData.get("set"));
-			window.location.reload();
-		});
-	}
+    this.el.addEventListener('submit', event => {
+      event.preventDefault();
+      const formData = new FormData(this.el);
+      localStorage.setItem('locale', formData.get('locale'));
+      localStorage.setItem('theme', formData.get('theme'));
+      localStorage.setItem('format', formData.get('format'));
+      localStorage.setItem('notation', formData.get('notation'));
+      localStorage.setItem('set', formData.get('set'));
+      window.location.reload();
+    });
+  }
 }
 
-export const settingsForm = new SettingsForm(
-	document.getElementById("settingsForm")
-);
+export const settingsForm = new SettingsForm(document.getElementById('settingsForm'));
