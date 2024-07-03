dc86efb709d670fffb6363c59106bb5aa9ecaceb
programarivm <info@programarivm.com>
2024-07-03 12:08:58 +0200

    Updated translations

--- assets/js/translations/messages.de.js
+++ assets/js/translations/messages.de.js
@@ -2,23 +2,20 @@ const messagesDe = {
   // Abstract
   "Draw by the fifty-move rule": "Remis nach der Fünfzig-Züge-Regel",
   "Draw by stalemate": "Remis durch Patt",
+  "Draw by fivefold repetition": "Remis durch fünffache Wiederholung",
   // Play
   "Waiting for player to join": "Warten auf Spielerbeitritt",
   // PlayWebSocket
-  "Waiting for your opponent to accept or decline":
-    "Warten auf die Annahme oder Ablehnung durch den Gegner",
-  "Invalid invite code, please try again":
-    "Ungültiger Einladungscode, bitte erneut versuchen",
+  "Waiting for your opponent to accept or decline": "Warten auf die Annahme oder Ablehnung durch den Gegner",
+  "Invalid invite code, please try again": "Ungültiger Einladungscode, bitte erneut versuchen",
   "Takeback declined": "Zurücknahme abgelehnt",
   "Takeback accepted": "Zurücknahme akzeptiert",
   "Draw offer declined": "Remisangebot abgelehnt",
   "Draw offer accepted": "Remisangebot akzeptiert",
   "The game is resigned": "Die Partie wurde aufgegeben",
   "Your opponent is gone": "Dein Gegner ist verschwunden",
-  "Draw by fivefold repetition": "Remis durch fünffache Wiederholung",
   // SanWebSocket
-  "Invalid SAN movetext, please try again":
-    "Ungültiger SAN-Zugtext, bitte erneut versuchen",
+  "Invalid SAN movetext, please try again": "Ungültiger SAN-Zugtext, bitte erneut versuchen"
 };
 
 export default messagesDe;
--- assets/js/translations/messages.es.js
+++ assets/js/translations/messages.es.js
@@ -1,25 +1,21 @@
 const messagesEs = {
   // Abstract
-  "Draw by the fifty-move rule":
-    "Empate según la regla de los cincuenta movimientos",
+  "Draw by the fifty-move rule": "Empate según la regla de los cincuenta movimientos",
   "Draw by stalemate": "Tablas por ahogado",
+  "Draw by fivefold repetition": "Empate por quíntuple repetición",
   // Play
   "Waiting for player to join": "Esperando a que el jugador o jugadora se una",
   // PlayWebSocket
-  "Waiting for your opponent to accept or decline":
-    "Espera a que tu oponente acepte o rechace",
-  "Invalid invite code, please try again":
-    "Código de invitación no válido, inténtalo de nuevo",
+  "Waiting for your opponent to accept or decline": "Espera a que tu oponente acepte o rechace",
+  "Invalid invite code, please try again": "Código de invitación no válido, inténtalo de nuevo",
   "Takeback declined": "Corrección del movimiento declinada",
   "Takeback accepted": "Corrección del movimiento acceptada",
   "Draw offer declined": "Oferta de tablas declinada",
   "Draw offer accepted": "Oferta de tablas aceptada",
   "The game is resigned": "Partida abandonada",
   "Your opponent is gone": "Tu oponente se ha ido",
-  "Draw by fivefold repetition": "Dibujar por quíntuple repetición",
   // SanWebSocket
-  "Invalid SAN movetext, please try again":
-    "Texto SAN con movimientos no válido, inténtalo de nuevo",
+  "Invalid SAN movetext, please try again": "Texto SAN con movimientos no válido, inténtalo de nuevo"
 };
 
 export default messagesEs;
