6795a8d27b2b29b576e4b9fd5ff6a3ddb18cec03
programarivm <info@programarivm.com>
2024-07-07 14:11:47 +0200

    Updated i18n.js

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -1,17 +1,20 @@
+import messagesDe from './translations/messages.de.js';
 import messagesEs from './translations/messages.es.js';
 import messagesFr from './translations/messages.fr.js';
+import messagesHi from './translations/messages.hi.js';
 import messagesTr from './translations/messages.tr.js';
-import messagesDe from './translations/messages.de.js';
 
 export const trans = (string) => {
-  if (document.documentElement.lang === 'es') {
+  if (document.documentElement.lang === 'de') {
+    return messagesDe[string];
+  } else if (document.documentElement.lang === 'es') {
     return messagesEs[string];
   } else if (document.documentElement.lang === 'fr') {
     return messagesFr[string];
+  } else if (document.documentElement.lang === 'hi') {
+    return messagesHi[string];
   } else if (document.documentElement.lang === 'tr') {
     return messagesTr[string];
-  } else if (document.documentElement.lang === 'de') {
-    return messagesDe[string];
   }
 
   return string;
