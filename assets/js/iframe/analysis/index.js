e8e7ced2fe4c1784cd978234a1ee3bdc364b0add
programarivm <info@programarivm.com>
2024-07-23 17:23:09 +0200

    Added index.js

---
+++ assets/js/iframe/analysis/index.js
@@ -0,0 +1,11 @@
+const variant = document.getElementById('analysis').dataset.variant;
+const fen = document.getElementById('analysis').dataset.fen;
+const movetext = document.getElementById('analysis').dataset.movetext;
+const startPos = document.getElementById('analysis').dataset.startpos;
+
+console.log(variant);
+console.log(fen);
+console.log(movetext);
+console.log(startPos);
+
+// TODO ...
