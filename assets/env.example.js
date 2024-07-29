7e8319c57393db34c75caf3dfa82ba576bdef7a4
programarivm <info@programarivm.com>
2024-07-29 13:13:02 +0200

    Updated the environment variables

--- assets/env.example.js
+++ assets/env.example.js
@@ -6,16 +6,10 @@ const WEBSOCKET_HOSTS = [
   'wss://async.chesslablab.org:8443'
 ];
 
-const API_SCHEME = 'https';
-const API_HOST = 'api.chesslablab.org';
-const API_PORT = '9443';
 const API_VERSION = 'v1';
 
 export {
-  API_SCHEME,
-  API_HOST,
   API_HOSTS,
-  API_PORT,
   API_VERSION,
   WEBSOCKET_HOSTS
 };
