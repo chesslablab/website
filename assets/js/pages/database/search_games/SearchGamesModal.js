606733fb3a772ddc8eed14ab6ec0a9ca49e63db3
programarivm <info@programarivm.com>
2024-08-06 22:41:32 +0200

    Refactored SearchGamesModal

--- assets/js/pages/database/search_games/SearchGamesModal.js
+++ assets/js/pages/database/search_games/SearchGamesModal.js
@@ -7,6 +7,7 @@ import { progressModal } from '../../ProgressModal.js';
 import { whiteAutocomplete } from '../../WhiteAutocomplete.js';
 import AbstractComponent from '../../../AbstractComponent.js';
 import { analysisWebSocket } from '../../../websockets/game/AnalysisWebSocket.js';
+import { dataWebSocket } from '../../../websockets/data/DataWebSocket.js';
 import * as connect from '../../../../connect.js';
 import * as env from '../../../../env.js';
 import * as mode from '../../../../mode.js';
@@ -20,62 +21,63 @@ export class SearchGamesModal extends AbstractComponent {
         event.preventDefault();
         this.props.progressModal.props.modal.show();
         const formData = new FormData(this.props.form);
-        const res = await fetch(`${connect.api()}/search`, {
-          method: 'POST',
-          body: JSON.stringify({
-            Event: formData.get('Event'),
-            Date: formData.get('Date'),
-            White: formData.get('White'),
-            Black: formData.get('Black'),
-            Result: formData.get('Result'),
-            ECO: formData.get('ECO'),
-            movetext: formData.get('movetext')
-          })
-        });
-        const tbody = this.props.form.getElementsByTagName('tbody')[0];
-        tbody.parentNode.classList.add('mt-3');
-        tbody.replaceChildren();
-        (await res.json()).forEach(game => {
-          const tr = document.createElement('tr');
-          const eventTd = document.createElement('td');
-          const yearTd = document.createElement('td');
-          const ecoTd = document.createElement('td');
-          const whiteTd = document.createElement('td');
-          const whiteEloTd = document.createElement('td');
-          const blackTd = document.createElement('td');
-          const blackEloTd = document.createElement('td');
-          const resultTd = document.createElement('td');
+        const settings = {
+          Event: formData.get('Event'),
+          Date: formData.get('Date'),
+          White: formData.get('White'),
+          Black: formData.get('Black'),
+          Result: formData.get('Result'),
+          ECO: formData.get('ECO'),
+          movetext: formData.get('movetext')
+        };
+        await dataWebSocket.connect();
+        dataWebSocket.send(`/search "${JSON.stringify(settings).replace(/"/g, '\\"')}"`);
+        dataWebSocket.watchResponse('/search', (newValue, oldValue) => {
+          const tbody = this.props.form.getElementsByTagName('tbody')[0];
+          tbody.parentNode.classList.add('mt-3');
+          tbody.replaceChildren();
+          newValue.forEach(game => {
+            const tr = document.createElement('tr');
+            const eventTd = document.createElement('td');
+            const yearTd = document.createElement('td');
+            const ecoTd = document.createElement('td');
+            const whiteTd = document.createElement('td');
+            const whiteEloTd = document.createElement('td');
+            const blackTd = document.createElement('td');
+            const blackEloTd = document.createElement('td');
+            const resultTd = document.createElement('td');
 
-          eventTd.appendChild(document.createTextNode(game.Event));
-          yearTd.appendChild(document.createTextNode(parseInt(game.Date)));
-          ecoTd.appendChild(document.createTextNode(game.ECO));
-          whiteTd.appendChild(document.createTextNode(game.White));
-          whiteEloTd.appendChild(document.createTextNode(game.WhiteElo));
-          blackTd.appendChild(document.createTextNode(game.Black));
-          blackEloTd.appendChild(document.createTextNode(game.BlackElo));
-          resultTd.appendChild(document.createTextNode(game.Result));
+            eventTd.appendChild(document.createTextNode(game.Event));
+            yearTd.appendChild(document.createTextNode(parseInt(game.Date)));
+            ecoTd.appendChild(document.createTextNode(game.ECO));
+            whiteTd.appendChild(document.createTextNode(game.White));
+            whiteEloTd.appendChild(document.createTextNode(game.WhiteElo));
+            blackTd.appendChild(document.createTextNode(game.Black));
+            blackEloTd.appendChild(document.createTextNode(game.BlackElo));
+            resultTd.appendChild(document.createTextNode(game.Result));
 
-          tr.appendChild(eventTd);
-          tr.appendChild(yearTd);
-          tr.appendChild(ecoTd);
-          tr.appendChild(whiteTd);
-          tr.appendChild(whiteEloTd);
-          tr.appendChild(blackTd);
-          tr.appendChild(blackEloTd);
-          tr.appendChild(resultTd);
+            tr.appendChild(eventTd);
+            tr.appendChild(yearTd);
+            tr.appendChild(ecoTd);
+            tr.appendChild(whiteTd);
+            tr.appendChild(whiteEloTd);
+            tr.appendChild(blackTd);
+            tr.appendChild(blackEloTd);
+            tr.appendChild(resultTd);
 
-          tr.addEventListener('click', event => {
-            const settings = {
-              movetext: game.movetext
-            };
-            analysisWebSocket.send(`/start classical ${mode.ANALYSIS} "${JSON.stringify(settings).replace(/"/g, '\\"')}"`);
-            this.props.movesMetadataTable.props = game;
-            this.props.movesMetadataTable.mount();
-            this.props.modal.hide();
+            tr.addEventListener('click', event => {
+              const settings = {
+                movetext: game.movetext
+              };
+              analysisWebSocket.send(`/start classical ${mode.ANALYSIS} "${JSON.stringify(settings).replace(/"/g, '\\"')}"`);
+              this.props.movesMetadataTable.props = game;
+              this.props.movesMetadataTable.mount();
+              this.props.modal.hide();
+            });
+
+            tbody.appendChild(tr);
           });
-
-          tbody.appendChild(tr);
-        })
+        });
       } catch (error) {
       } finally {
         this.props.progressModal.props.modal.hide();
