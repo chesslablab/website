362d9ad872dd1f519e867f7ed46a920e796edae5
programarivm <info@programarivm.com>
2024-08-07 12:49:08 +0200

    Refactored EventAutocomplete

--- assets/js/pages/EventAutocomplete.js
+++ assets/js/pages/EventAutocomplete.js
@@ -1,6 +1,5 @@
 import AbstractComponent from '../AbstractComponent.js';
-import * as connect from '../../connect.js';
-import * as env from '../../env.js';
+import { dataWebSocket } from '../websockets/data/DataWebSocket.js';
 
 export class EventAutocomplete extends AbstractComponent {
   mount() {
@@ -10,21 +9,22 @@ export class EventAutocomplete extends AbstractComponent {
         if (event.target.value.length % 3 === 0) {
           this.props.submitButton.classList.add('d-none');
           this.props.loadingButton.classList.remove('d-none');
-          const res = await fetch(`${connect.api()}/autocomplete/event`, {
-            method: 'POST',
-            body: JSON.stringify({
-              Event: event.target.value
-            })
-          });
-          this.props.datalist.replaceChildren();
-          (await res.json()).forEach(item => {
-            const option = document.createElement('option');
-            option.appendChild(document.createTextNode(item));
-            option.addEventListener('click', (event) => {
-              event.preventDefault();
-              this.el.value = item;
+          const settings = {
+            Event: event.target.value
+          };
+          await dataWebSocket.connect();
+          dataWebSocket.send(`/autocomplete_event "${JSON.stringify(settings).replace(/"/g, '\\"')}"`);
+          dataWebSocket.watch('/autocomplete_event', (newValue, oldValue) => {
+            this.props.datalist.replaceChildren();
+            newValue.forEach(item => {
+              const option = document.createElement('option');
+              option.appendChild(document.createTextNode(item));
+              option.addEventListener('click', (event) => {
+                event.preventDefault();
+                this.el.value = item;
+              });
+              this.props.datalist.append(option);
             });
-            this.props.datalist.append(option);
           });
         }
       } catch (error) {
