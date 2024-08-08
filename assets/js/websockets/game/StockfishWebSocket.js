9e6996030d6362d3785e29af318f6db23e737468
programarivm <info@programarivm.com>
2024-08-08 15:46:58 +0200

    Updated StockfishWebSocket

--- assets/js/websockets/game/StockfishWebSocket.js
+++ assets/js/websockets/game/StockfishWebSocket.js
@@ -22,67 +22,72 @@ export class StockfishWebSocket extends AbstractGameWebSocket {
         case '/start':
           this.chessboard.disableMoveInput();
           this.chessboard.enableMoveInput(event => this.inputHandler(event));
-          this.chessboard.setPosition(data[msg].fen, true);
-          if (data[msg].color === COLOR.black) {
+          this.chessboard.setPosition(this._response[msg].fen, true);
+          if (this._response[msg].color === COLOR.black) {
             this.chessboard.setOrientation(COLOR.black);
           }
-          if (data[msg].fen.split(' ')[1] !== data[msg].color) {
+          if (this._response[msg].fen.split(' ')[1] !== this._response[msg].color) {
             this.send(`/stockfish "{\\"Skill Level\\":${sessionStorage.getItem('skillLevel')}}" "{\\"depth\\":12}"`);
           }
           break;
 
         case '/legal':
-          data[msg].forEach(sq => {
+          this._response[msg].forEach(sq => {
             this.chessboard.addMarker(MARKER_TYPE.dot, sq);
           });
           break;
 
         case '/play_lan':
-          if (data[msg].isValid) {
-            this.chessboard.setPosition(data[msg].fen, true);
+          if (this._response[msg].isValid) {
+            this.chessboard.setPosition(this._response[msg].fen, true);
             stockfishPanel.props.sanMovesBrowser.current = stockfishPanel.props.sanMovesBrowser.props.fen.length;
-            stockfishPanel.props.sanMovesBrowser.props.movetext = Movetext.notation(localStorage.getItem('notation'), data[msg].movetext);
-            stockfishPanel.props.sanMovesBrowser.props.fen = stockfishPanel.props.sanMovesBrowser.props.fen.concat(data[msg].fen);
+            stockfishPanel.props.sanMovesBrowser.props.movetext
+              = Movetext.notation(localStorage.getItem('notation'), this._response[msg].movetext);
+            stockfishPanel.props.sanMovesBrowser.props.fen
+              = stockfishPanel.props.sanMovesBrowser.props.fen.concat(this._response[msg].fen);
             stockfishPanel.props.sanMovesBrowser.mount();
-            stockfishPanel.props.openingTable.props.movetext = data[msg].movetext;
+            stockfishPanel.props.openingTable.props.movetext = this._response[msg].movetext;
             stockfishPanel.props.openingTable.mount();
-            if (!this._gameOver(data[msg])) {
+            if (!this._gameOver(this._response[msg])) {
               this.send(`/stockfish "{\\"Skill Level\\":${sessionStorage.getItem('skillLevel')}}" "{\\"depth\\":12}"`);
             }
           } else {
-            this.chessboard.setPosition(data[msg].fen, false);
+            this.chessboard.setPosition(this._response[msg].fen, false);
           }
           break;
 
         case '/undo':
-          this.chessboard.setPosition(data[msg].fen, true);
-          if (!data[msg].movetext) {
+          this.chessboard.setPosition(this._response[msg].fen, true);
+          if (!this._response[msg].movetext) {
             this.chessboard.state.inputWhiteEnabled = true;
             this.chessboard.state.inputBlackEnabled = false;
           }
           stockfishPanel.props.sanMovesBrowser.current -= 1;
           stockfishPanel.props.sanMovesBrowser.props.fen.splice(-1);
-          stockfishPanel.props.sanMovesBrowser.props.movetext = Movetext.notation(localStorage.getItem('notation'), data[msg].movetext);
+          stockfishPanel.props.sanMovesBrowser.props.movetext
+            = Movetext.notation(localStorage.getItem('notation'), this._response[msg].movetext);
           stockfishPanel.props.sanMovesBrowser.mount();
-          stockfishPanel.props.openingTable.props.movetext = data[msg].movetext;
+          stockfishPanel.props.openingTable.props.movetext = this._response[msg].movetext;
           stockfishPanel.props.openingTable.mount();
           break;
 
         case '/stockfish':
-          this.chessboard.setPosition(data[msg].fen, true);
+          this.chessboard.setPosition(this._response[msg].fen, true);
           stockfishPanel.props.sanMovesBrowser.current = stockfishPanel.props.sanMovesBrowser.props.fen.length;
-          stockfishPanel.props.sanMovesBrowser.props.movetext = Movetext.notation(localStorage.getItem('notation'), data[msg].movetext);
-          stockfishPanel.props.sanMovesBrowser.props.fen = stockfishPanel.props.sanMovesBrowser.props.fen.concat(data[msg].fen);
+          stockfishPanel.props.sanMovesBrowser.props.movetext
+            = Movetext.notation(localStorage.getItem('notation'), this._response[msg].movetext);
+          stockfishPanel.props.sanMovesBrowser.props.fen
+            = stockfishPanel.props.sanMovesBrowser.props.fen.concat(this._response[msg].fen);
           stockfishPanel.props.sanMovesBrowser.mount();
-          stockfishPanel.props.openingTable.props.movetext = data[msg].movetext;
+          stockfishPanel.props.openingTable.props.movetext = this._response[msg].movetext;
           stockfishPanel.props.openingTable.mount();
-          this._gameOver(data[msg]);
+          this._gameOver(this._response[msg]);
           break;
 
         case '/randomizer':
           this.chessboard.state.inputWhiteEnabled = false;
           this.chessboard.state.inputBlackEnabled = false;
-          if (data[msg].turn === COLOR.white) {
+          if (this._response[msg].turn === COLOR.white) {
             this.chessboard.state.inputWhiteEnabled = true;
           } else {
             this.chessboard.state.inputBlackEnabled = true;
@@ -90,8 +95,8 @@ export class StockfishWebSocket extends AbstractGameWebSocket {
           sessionStorage.setItem('skillLevel', 20);
           sessionStorage.setItem('depth', 12);
           const settings = {
-            color: data[msg].turn,
-            fen: data[msg].fen
+            color: this._response[msg].turn,
+            fen: this._response[msg].fen
           };
           this.send(`/start ${variant.CLASSICAL} ${mode.STOCKFISH} "${JSON.stringify(settings).replace(/"/g, '\\"')}"`);
           break;
