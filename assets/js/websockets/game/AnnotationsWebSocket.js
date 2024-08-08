1b0238deb243fd0af6724aa32e13eab9ccb03e65
programarivm <info@programarivm.com>
2024-08-08 17:18:12 +0200

    Refactored AnnotationsWebSocket

--- assets/js/websockets/game/AnnotationsWebSocket.js
+++ assets/js/websockets/game/AnnotationsWebSocket.js
@@ -1,24 +1,6 @@
 import AbstractGameWebSocket from './AbstractGameWebSocket.js';
-import * as connect from '../../../connect.js';
 
 export class AnnotationsWebSocket extends AbstractGameWebSocket {
-  async connect() {
-    await super.connect(connect.wsGame());
-
-    this._socket.onmessage = (res) => {
-      const data = JSON.parse(res.data);
-      const msg = Object.keys(data)[0];
-      this._response[msg] = data[msg];
-      switch (msg) {
-        case 'error':
-          console.log('Whoops! Something went wrong.');
-          break;
-
-        default:
-          break;
-      }
-    };
-  }
 }
 
 export const annotationsWebSocket = new AnnotationsWebSocket();
