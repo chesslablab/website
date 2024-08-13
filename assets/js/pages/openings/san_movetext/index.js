db6de9b7ea46771a61576c330d333fc96eb8e467
programarivm <info@programarivm.com>
2024-08-13 21:06:23 +0200

    Deleted openings by movetext

--- assets/js/pages/openings/san_movetext/index.js
+++
@@ -1,8 +0,0 @@
-import { openingsSanMovetextModal } from './OpeningsSanMovetextModal.js';
-import { analysisWebSocket } from '../../../websockets/game/AnalysisWebSocket.js';
-
-sessionStorage.clear();
-
-await analysisWebSocket.connect();
-
-openingsSanMovetextModal.props.modal.show();
