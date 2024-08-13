49c98ab50a673b39952d0494d7551e8641f31847
programarivm <info@programarivm.com>
2024-08-13 23:26:31 +0200

    Refactored the openings search

--- assets/js/pages/openings/search/OpeningsSearchModal.js
+++ assets/js/pages/openings/search/OpeningsSearchModal.js
@@ -5,7 +5,21 @@ import AbstractComponent from '../../../AbstractComponent.js';
 
 export class OpeningsSearchModal extends AbstractComponent {
   mount() {
-    this.props.form.addEventListener('submit', event => {
+    this.props.form.querySelector('select[name="eco"]').addEventListener('change', event => {
+      event.preventDefault();
+      const openingsTable = new OpeningsTable(
+        this.props.form.querySelector('table'),
+        {
+          modal: this.props.modal,
+          openings: Opening.byEco(event.target.value)
+        }
+      );
+      openingsTable.mount();
+      this.props.form.querySelector('input[name="name"]').value = '';
+      this.props.form.querySelector('input[name="movetext"]').value = '';
+    });
+
+    this.el.addEventListener('submit', event => {
       event.preventDefault();
       const formData = new FormData(this.props.form);
       const openingsTable = new OpeningsTable(
@@ -13,7 +27,10 @@ export class OpeningsSearchModal extends AbstractComponent {
         {
           modal: this.props.modal,
           openings: Opening.byName(formData.get('name')).filter(opening => {
-            if (opening.movetext.startsWith(formData.get('movetext'))) {
+            if (
+              opening.eco.startsWith(formData.get('eco')) &&
+              opening.movetext.startsWith(formData.get('movetext'))
+            ) {
               return opening;
             }
           })
