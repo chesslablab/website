135e87cda777d83436119ff87f2694deb2034c79
programarivm <info@programarivm.com>
2024-08-13 10:13:37 +0200

    Refactored AnnotationsWebSocket

--- assets/js/websockets/game/AnnotationsWebSocket.js
+++ assets/js/websockets/game/AnnotationsWebSocket.js
@@ -1,16 +1,23 @@
 import GameWebSocket from './GameWebSocket.js';
 import { ravPanel } from '../../pages/RavPanel.js';
 
-export const annotationsWebSocket = new GameWebSocket()
-  .watch('/play_rav', data => {
-    ravPanel.props.ravMovesBrowser.current = data.fen.length - 1;
-    ravPanel.props.ravMovesBrowser.props.chessboard.setPosition(data.fen[data.fen.length - 1]);
-    ravPanel.props.ravMovesBrowser.props = {
-      ...ravPanel.props.ravMovesBrowser.props,
-      filtered: data.filtered,
-      breakdown: data.breakdown,
-      fen: data.fen
-    };
-    ravPanel.props.ravMovesBrowser.mount();
-    ravPanel.progressModal.props.modal.hide();
-  });
+export class AnnotationsWebSocket extends GameWebSocket {
+  constructor() {
+    super();
+
+    this.watch('/play_rav', data => {
+      ravPanel.props.ravMovesBrowser.current = data.fen.length - 1;
+      ravPanel.props.ravMovesBrowser.props.chessboard.setPosition(data.fen[data.fen.length - 1]);
+      ravPanel.props.ravMovesBrowser.props = {
+        ...ravPanel.props.ravMovesBrowser.props,
+        filtered: data.filtered,
+        breakdown: data.breakdown,
+        fen: data.fen
+      };
+      ravPanel.props.ravMovesBrowser.mount();
+      ravPanel.progressModal.props.modal.hide();
+    });
+  }
+}
+
+export const annotationsWebSocket = new AnnotationsWebSocket();
