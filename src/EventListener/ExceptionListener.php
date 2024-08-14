fc3cde53fab087f4a5e15b13e073b99905348341
programarivm <info@programarivm.com>
2024-08-14 12:17:18 +0200

    Added NotTranslatedException

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -2,26 +2,35 @@
 
 namespace App\EventListener;
 
+use App\Exception\NotTranslatedException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Twig\Environment;
 
 class ExceptionListener
 {
-    public function __invoke(ExceptionEvent $event): void
+    private Environment $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function __invoke(ExceptionEvent $event)
     {
         $exception = $event->getThrowable();
-        $message = $exception->getMessage();
 
         $response = new Response();
-        $response->setContent($message);
 
-        if ($exception instanceof HttpExceptionInterface) {
-            $response->setStatusCode($exception->getStatusCode());
-            $response->headers->replace($exception->getHeaders());
+        if ($exception instanceof NotTranslatedException) {
+            $response->setStatusCode(Response::HTTP_NOT_FOUND);
+            $html = $this->twig->render('error/404/notTranslated.html.twig');
+            $response->setContent($html);
         } else {
             $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
         }
+
 
         $event->setResponse($response);
     }
