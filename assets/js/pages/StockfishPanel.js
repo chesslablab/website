94cf87a72d02f91c134be80287220e5f2ebb2bbd
programarivm <info@programarivm.com>
2024-08-14 23:53:32 +0200

    Fixed StockfishPanel

--- assets/js/pages/StockfishPanel.js
+++ assets/js/pages/StockfishPanel.js
@@ -4,13 +4,14 @@ import historyButtons from './historyButtons.js';
 import openingTable from './openingTable.js';
 import sanMovesBrowser from './sanMovesBrowser.js';
 import AbstractComponent from '../AbstractComponent.js';
+import { stockfishWebSocket } from '../websockets/game/StockfishWebSocket.js';
 
 export class StockfishPanel extends AbstractComponent {
   mount() {
     this.props.gameActionsDropdown.props.ul.children.item(0).addEventListener('click', (event) => {
       event.preventDefault();
-      analysisWebSocket.send('/undo');
-      analysisWebSocket.send('/undo');
+      stockfishWebSocket.send('/undo');
+      stockfishWebSocket.send('/undo');
     });
   }
 }
