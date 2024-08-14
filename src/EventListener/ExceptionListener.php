c5183a3a4309ed755bb0b0c05de9d95003c072a9
programarivm <info@programarivm.com>
2024-08-14 10:26:28 +0200

    Implemented an exception event listener

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ExceptionListener
+{
+    public function __invoke(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        $message = $exception->getMessage();
+
+        $response = new Response();
+        $response->setContent($message);
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+            $response->headers->replace($exception->getHeaders());
+        } else {
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        $event->setResponse($response);
+    }
+}
