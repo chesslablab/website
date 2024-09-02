ff39a46cb7d0bef7c972d63a05f29bfef7692afe
programarivm <info@programarivm.com>
2024-09-02 20:28:44 +0200

    Set cookie

--- src/Controller/Pages/SignIn/AuthenticateController.php
+++ src/Controller/Pages/SignIn/AuthenticateController.php
@@ -5,14 +5,27 @@ namespace App\Controller\Pages\SignIn;
 use OTPHP\InternalClock;
 use OTPHP\TOTP;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthenticateController extends AbstractController
 {
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        echo 'TODO ...';
+        $otp = TOTP::createFromSecret($_ENV['TOTP_SECRET'], new InternalClock());
+        $otp->setDigits(9);
 
-        exit;
+        if ($otp->verify($request->request->get('password'), null, 5)) {
+            $cookie = Cookie::create('username', $request->request->get('username'));
+            $response = new RedirectResponse($this->generateUrl('pages_play_online'));
+            $response->headers->setCookie($cookie);
+            return $response;
+        }
+
+        $response = new RedirectResponse($this->generateUrl('pages_sign_in'));
+        $response->headers->clearCookie('username');
+        return $response;
     }
 }
