48949be6d1983007859c6fa02f91b0e54519ba48
programarivm <info@programarivm.com>
2024-09-02 17:43:00 +0200

    Implemented RegisterController

--- src/Controller/Pages/SignIn/RegisterController.php
+++ src/Controller/Pages/SignIn/RegisterController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller\Pages\SignIn;
 
+use App\Utils\RandomUsername;
+use OTPHP\InternalClock;
+use OTPHP\TOTP;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -9,6 +12,19 @@ class RegisterController extends AbstractController
 {
     public function index(): Response
     {
-        return $this->render('pages/sign_in/register/index.html.twig');
+        $username = (new RandomUsername())->generate();
+
+        $otp = TOTP::createFromSecret($_ENV['TOTP_SECRET'], new InternalClock());
+        $otp->setDigits(9);
+        $otp->setLabel($username);
+        $otp->setIssuer('ChesslaBlab');
+        $otp->setParameter('image', 'https://chesslablab.org/logo.png');
+
+        $grCodeUri = $otp->getQrCodeUri(
+            'https://api.qrserver.com/v1/create-qr-code/?data=[DATA]&size=300x300&ecc=M',
+            '[DATA]'
+        );
+
+        return $this->render('pages/sign_in/register/index.html.twig', ['username' => $username, 'grCodeUri' => $grCodeUri]);
     }
 }
