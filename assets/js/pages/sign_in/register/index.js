2d91b33b6ce1e339ee022ce2ca9ff87e4399d417
programarivm <info@programarivm.com>
2024-09-03 17:18:07 +0200

    Display the QR code for registration

--- assets/js/pages/sign_in/register/index.js
+++ assets/js/pages/sign_in/register/index.js
@@ -1,1 +1,15 @@
-console.log('TODO');
+import { dataWebSocket } from '../../../websockets/data/DataWebSocket.js';
+
+sessionStorage.clear();
+
+try {
+  await dataWebSocket.connect();
+} catch {}
+
+dataWebSocket
+  .send(`/qr`)
+  .onChange('/qr', data => {
+    const img = document.createElement('img');
+    img.src = data.uri;
+    document.getElementById('qrCodeUri').appendChild(img);
+  });
