71a82b5e4e7c4567059f2f663c2e8c3e5d0b786f
programarivm <info@programarivm.com>
2024-09-04 17:53:29 +0200

    Sent the username to the chess server

--- assets/js/pages/play/online/CreateGameModal.js
+++ assets/js/pages/play/online/CreateGameModal.js
@@ -1,3 +1,4 @@
+import jsCookie from 'https://cdn.jsdelivr.net/npm/js-cookie@3.0.5/+esm';
 import Modal from 'bootstrap/js/dist/modal.js';
 import AbstractComponent from '../../../AbstractComponent.js';
 import { playWebSocket } from '../../../websockets/game/PlayWebSocket.js';
@@ -12,7 +13,8 @@ export class CreateGameModal extends AbstractComponent {
         min: formData.get('minutes'),
         increment: formData.get('increment'),
         color: formData.get('color'),
-        submode: 'online'
+        submode: 'online',
+        username: jsCookie.get('ui') ? JSON.parse(jsCookie.get('ui')).username : null
       };
       sessionStorage.setItem('color', formData.get('color'));
       playWebSocket.send(`/start ${formData.get('variant')} ${mode.PLAY} "${JSON.stringify(settings).replace(/"/g, '\\"')}"`);
