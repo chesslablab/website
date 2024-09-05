a8cb27661a278a9c9c6361bc5e50cbc3c2b84dde
orestonce <orestonce@163.com>
2024-09-06 07:07:15 +0800

    translation zh_CN

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -3,6 +3,7 @@ import messagesEs from './translations/messages.es.js';
 import messagesFr from './translations/messages.fr.js';
 import messagesHi from './translations/messages.hi.js';
 import messagesTr from './translations/messages.tr.js';
+import messagesTr from './translations/messages.zh_CN.js';
 
 export const trans = (string) => {
   if (document.documentElement.lang === 'de') {
@@ -15,6 +16,8 @@ export const trans = (string) => {
     return messagesHi[string];
   } else if (document.documentElement.lang === 'tr') {
     return messagesTr[string];
+  } else if (document.documentElement.lang === 'zh_CN') {
+    return messagesZh_CN[string];
   }
 
   return string;
---
+++ assets/js/translations/messages.zh_CN.js
@@ -0,0 +1,28 @@
+const messagesZh = {
+  // AbstractWebSocket
+  "Whoops! We are experiencing some technical issues": "哎呀！我们遇到了一些技术问题",
+  // GameWebSocket
+  "It's a win": "赢了",
+  "It's a draw": "平局",
+  "White wins": "白方获胜",
+  "Black wins": "黑方获胜",
+  "Draw by stalemate": "因逼和而平局",
+  "Draw by fivefold repetition": "因五次重复局面而平局",
+  "Draw by the fifty-move rule": "因五十步规则而平局",
+  "Draw by dead position": "因死局而平局",
+  // Play
+  "Waiting for player to join": "等待玩家加入",
+  // PlayWebSocket
+  "Waiting for your opponent to accept or decline": "等待对手接受或拒绝",
+  "Invalid invite code, please try again": "邀请码无效，请重试",
+  "Takeback declined": "悔棋被拒绝",
+  "Takeback accepted": "悔棋被接受",
+  "Draw offer declined": "和棋提议被拒绝",
+  "Draw offer accepted": "和棋提议被接受",
+  "The game is resigned": "游戏认输",
+  "Your opponent is gone": "你的对手已离开",
+  // AnalysisWebSocket
+  "Invalid SAN movetext, please try again": "SAN 记谱法无效，请重试"
+};
+
+export default messagesZh_CN;
