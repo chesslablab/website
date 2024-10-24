afaf243536d5bbb227e5cd3b815e65682a3af90b
programarivm <info@programarivm.com>
2024-10-24 12:10:11 +0200

    Ordered the entry points alphabetically

--- importmap.php
+++ importmap.php
@@ -9,44 +9,36 @@ return [
         'path' => './assets/js/iframes/annotations/index.js',
         'entrypoint' => true,
     ],
-    'js/pages/play/online/index.js' => [
-        'path' => './assets/js/pages/play/online/index.js',
-        'entrypoint' => true,
-    ],
-    'js/pages/play/computer/index.js' => [
-        'path' => './assets/js/pages/play/computer/index.js',
-        'entrypoint' => true,
-    ],
-    'js/pages/home/index.js' => [
-        'path' => './assets/js/pages/home/index.js',
-        'entrypoint' => true,
-    ],
-    'js/pages/learn/rav_movetext/index.js' => [
-        'path' => './assets/js/pages/learn/rav_movetext/index.js',
-        'entrypoint' => true,
-    ],
-    'js/pages/learn/annotated_games/index.js' => [
-        'path' => './assets/js/pages/learn/annotated_games/index.js',
+    'js/pages/edit/index.js' => [
+        'path' => './assets/js/pages/edit/index.js',
         'entrypoint' => true,
     ],
     'js/pages/games/index.js' => [
         'path' => './assets/js/pages/games/index.js',
         'entrypoint' => true,
     ],
-    'js/pages/training/checkmate/index.js' => [
-        'path' => './assets/js/pages/training/checkmate/index.js',
+    'js/pages/home/index.js' => [
+        'path' => './assets/js/pages/home/index.js',
         'entrypoint' => true,
     ],
-    'js/pages/training/endgame/index.js' => [
-        'path' => './assets/js/pages/training/endgame/index.js',
+    'js/pages/learn/annotated_games/index.js' => [
+        'path' => './assets/js/pages/learn/annotated_games/index.js',
+        'entrypoint' => true,
+    ],
+    'js/pages/learn/rav_movetext/index.js' => [
+        'path' => './assets/js/pages/learn/rav_movetext/index.js',
+        'entrypoint' => true,
+    ],
+    'js/pages/play/computer/index.js' => [
+        'path' => './assets/js/pages/play/computer/index.js',
+        'entrypoint' => true,
+    ],
+    'js/pages/play/online/index.js' => [
+        'path' => './assets/js/pages/play/online/index.js',
         'entrypoint' => true,
     ],
     'js/pages/settings/index.js' => [
         'path' => './assets/js/pages/settings/index.js',
-        'entrypoint' => true,
-    ],
-    'js/pages/edit/index.js' => [
-        'path' => './assets/js/pages/edit/index.js',
         'entrypoint' => true,
     ],
     'js/pages/signin/index.js' => [
@@ -55,6 +47,14 @@ return [
     ],
     'js/pages/signin/register/index.js' => [
         'path' => './assets/js/pages/signin/register/index.js',
+        'entrypoint' => true,
+    ],
+    'js/pages/training/checkmate/index.js' => [
+        'path' => './assets/js/pages/training/checkmate/index.js',
+        'entrypoint' => true,
+    ],
+    'js/pages/training/endgame/index.js' => [
+        'path' => './assets/js/pages/training/endgame/index.js',
         'entrypoint' => true,
     ],
     '@chesslablab/chessboard' => [
