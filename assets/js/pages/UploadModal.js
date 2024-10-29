1b19aa350494065a835ac53035e666c307c5cd9f
programarivm <info@programarivm.com>
2024-10-29 13:30:59 +0100

    Implemented UploadModal

--- assets/js/pages/UploadModal.js
+++ assets/js/pages/UploadModal.js
@@ -2,10 +2,29 @@ import Modal from 'bootstrap/js/dist/modal.js';
 import BaseComponent from '../BaseComponent.js';
 
 export class UploadModal extends BaseComponent {
+  toBase64(file) {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+      fileReader.onload = () => {
+        resolve(fileReader.result);
+      };
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+  };
+
+  async upload(event) {
+    return await this.toBase64(event.target.files[0]);
+  };
+
   mount() {
-    this.el.addEventListener('submit', event => {
+    this.props.chessboardInput.addEventListener('change', event => {
       event.preventDefault();
-      // TODO
+      this.upload(event).then((data) => {
+        // TODO
+      });
     });
   }
 }
@@ -13,6 +32,7 @@ export class UploadModal extends BaseComponent {
 export const uploadModal = new UploadModal(
   document.getElementById('uploadModal'),
   {
-    modal: new Modal(document.getElementById('uploadModal'))
+    modal: new Modal(document.getElementById('uploadModal')),
+    chessboardInput: document.querySelector('input[name="chessboard"]')
   }
 );
