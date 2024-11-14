e77136ab2f9c5e9c48f6840efa5bdc49a1206964
programarivm <info@programarivm.com>
2024-11-14 19:01:26 +0100

    Implemented WebSockets

--- assets/js/pages/console/index.js
+++ assets/js/pages/console/index.js
@@ -5,11 +5,20 @@ import * as connect from '../../connect.js';
 class DataWebSocket extends AbstractWebSocket {
   static PORT = 9443;
   async connect() {
-    await super.connect(`${connect.ws()}:${DataWebSocket.PORT}`);
+    await super.connect(`${connect.ws()}:${GameWebSocket.PORT}`);
     this.socket.onmessage = (res) => {
       const data = JSON.parse(res.data);
       const msg = Object.keys(data)[0];
       this.response[msg] = data[msg];
+      switch (msg) {
+        case 'error':
+          consoleForm.print('Whoops! Something went wrong with this command.');
+          break;
+
+        default:
+          consoleForm.print(JSON.stringify(data[msg]));
+          break;
+      }
     };
   }
 }
@@ -35,13 +44,59 @@ class GameWebSocket extends AbstractWebSocket {
   }
 }
 
+class BinaryWebSocket extends AbstractWebSocket {
+  static PORT = 7443;
+  async connect() {
+    await super.connect(`${connect.ws()}:${GameWebSocket.PORT}`);
+    this.socket.onmessage = (res) => {
+      const data = JSON.parse(res.data);
+      const msg = Object.keys(data)[0];
+      this.response[msg] = data[msg];
+      switch (msg) {
+        case 'error':
+          consoleForm.print('Whoops! Something went wrong with this command.');
+          break;
+
+        default:
+          consoleForm.print(JSON.stringify(data[msg]));
+          break;
+      }
+    };
+  }
+}
+
+class AuthWebSocket extends AbstractWebSocket {
+  static PORT = 6443;
+  async connect() {
+    await super.connect(`${connect.ws()}:${GameWebSocket.PORT}`);
+    this.socket.onmessage = (res) => {
+      const data = JSON.parse(res.data);
+      const msg = Object.keys(data)[0];
+      this.response[msg] = data[msg];
+      switch (msg) {
+        case 'error':
+          consoleForm.print('Whoops! Something went wrong with this command.');
+          break;
+
+        default:
+          consoleForm.print(JSON.stringify(data[msg]));
+          break;
+      }
+    };
+  }
+}
+
 export const dataWebSocket = new DataWebSocket();
 export const gameWebSocket = new GameWebSocket();
+export const binaryWebSocket = new BinaryWebSocket();
+export const authWebSocket = new AuthWebSocket();
 
 try {
   await Promise.all([
     dataWebSocket.connect(),
-    gameWebSocket.connect()
+    gameWebSocket.connect(),
+    binaryWebSocket.connect(),
+    authWebSocket.connect()
   ]);
 } catch {}
 
