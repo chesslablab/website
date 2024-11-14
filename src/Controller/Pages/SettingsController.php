356e90fa964d1b97412ebd57b355f048f715d3f5
programarivm <info@programarivm.com>
2024-11-14 10:04:46 +0100

    A simpler settings page

--- src/Controller/Pages/SettingsController.php
+++ src/Controller/Pages/SettingsController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Pages\Settings;
+namespace App\Controller\Pages;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
