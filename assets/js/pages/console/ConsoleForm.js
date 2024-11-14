5f348bb0b5e56a6b01b8745cb5ec4936653ac041
programarivm <info@programarivm.com>
2024-11-14 18:30:56 +0100

    Implemented ConsoleForm

--- assets/js/pages/console/ConsoleForm.js
+++ assets/js/pages/console/ConsoleForm.js
@@ -1,8 +1,37 @@
+import { gameWebSocket } from './index.js';
 import BaseComponent from '../../BaseComponent.js';
 
 export class ConsoleForm extends BaseComponent {
   mount() {
     this.el.querySelector('textarea[name="command"]').focus();
+
+    this.el.querySelector('textarea[name="command"]').addEventListener('keydown', async (event) => {
+      if (event.keyCode === 13) {
+        let filtered = event.target.value.replace(/\s+/g, ' ');
+        let words = filtered.split(' ');
+        let command = words[0];
+        let params = null;
+
+        words = words.slice(1);
+        if (words.length > 1) {
+          params = JSON.parse(words.join(''));
+        } else if (words.length === 1) {
+          command += ` ${words[0]}`;
+        }
+
+        gameWebSocket.send(command, params);
+      }
+    });
+  }
+
+  print(msg) {
+    const commandP = document.createElement('p');
+    const responseP = document.createElement('p');
+    commandP.appendChild(document.createTextNode(this.el.querySelector('textarea[name="command"]').value));
+    responseP.appendChild(document.createTextNode(msg));
+    this.el.querySelector('div[id="response"]').appendChild(commandP);
+    this.el.querySelector('div[id="response"]').appendChild(responseP);
+    this.el.querySelector('textarea[name="command"]').value = '';
   }
 }
 
--- assets/js/pages/console/index.js
+++ assets/js/pages/console/index.js
@@ -1,1 +1,46 @@
 import { consoleForm } from './ConsoleForm.js';
+import AbstractWebSocket from '../../AbstractWebSocket.js';
+import * as connect from '../../connect.js';
+
+class DataWebSocket extends AbstractWebSocket {
+  static PORT = 9443;
+  async connect() {
+    await super.connect(`${connect.ws()}:${DataWebSocket.PORT}`);
+    this.socket.onmessage = (res) => {
+      const data = JSON.parse(res.data);
+      const msg = Object.keys(data)[0];
+      this.response[msg] = data[msg];
+    };
+  }
+}
+
+class GameWebSocket extends AbstractWebSocket {
+  static PORT = 8443;
+  async connect() {
+    await super.connect(`${connect.ws()}:${GameWebSocket.PORT}`);
+    this.socket.onmessage = (res) => {
+      const data = JSON.parse(res.data);
+      const msg = Object.keys(data)[0];
+      this.response[msg] = data[msg];
+      switch (msg) {
+        case 'error':
+          consoleForm.print('Whoops! Something went wrong with this command.');
+          break;
+
+        default:
+          consoleForm.print(JSON.stringify(data[msg]));
+          break;
+      }
+    };
+  }
+}
+
+export const dataWebSocket = new DataWebSocket();
+export const gameWebSocket = new GameWebSocket();
+
+try {
+  await Promise.all([
+    dataWebSocket.connect(),
+    gameWebSocket.connect()
+  ]);
+} catch {}
