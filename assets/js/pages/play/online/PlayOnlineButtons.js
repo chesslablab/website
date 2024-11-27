dd1711e95d9fb4d64458c3973be37773b592fb56
programarivm <info@programarivm.com>
2024-11-27 13:34:17 +0100

    Refactored PlayOnlineButtons

--- assets/js/pages/play/online/PlayOnlineButtons.js
+++ assets/js/pages/play/online/PlayOnlineButtons.js
@@ -1,13 +1,15 @@
-import BaseComponent from '../../../BaseComponent.js';
+import RootComponent from '../../../RootComponent.js';
 import { friendButtons } from './FriendButtons.js';
 import { playerButtons } from './PlayerButtons.js';
 import { playersButtons } from './PlayersButtons.js';
 
-export const playOnlineButtons = new BaseComponent(
-  document.querySelector('#playOnlineButtons'),
-  {
-    friendButtons: friendButtons,
-    playerButtons: playerButtons,
-    playersButtons: playersButtons
+export const playOnlineButtons = new RootComponent({
+  el: document.querySelector('#playOnlineButtons'),
+  props() {
+    return({
+      friendButtons: friendButtons,
+      playerButtons: playerButtons,
+      playersButtons: playersButtons
+    });
   }
-);
+});
