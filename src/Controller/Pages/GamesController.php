539a889605094e1ec6b7beb3c03dde6e8fc7e98a
programarivm <info@programarivm.com>
2024-11-29 15:15:29 +0100

    Added dynamic entrypoint to GamesController

--- src/Controller/Pages/GamesController.php
+++ src/Controller/Pages/GamesController.php
@@ -9,6 +9,8 @@ class GamesController extends AbstractController
 {
     public function index(): Response
     {
-        return $this->render('pages/games/index.html.twig');
+        return $this->render('pages/games/index.html.twig', [
+            'entrypoint' => 'js/pages/games/index.js',
+        ]);
     }
 }
