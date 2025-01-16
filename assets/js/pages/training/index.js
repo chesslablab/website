8e5635afbac0830120eb958c860c23fa41db345a
standard <info@programarivm.com>
2025-01-16 23:58:25 +0100

    Updated the Training page

--- assets/js/pages/training/index.js
+++ assets/js/pages/training/index.js
@@ -1,7 +1,67 @@
-import { checkmateForm } from './CheckmateForm.js';
-import { endgameForm } from './EndgameForm.js';
+import { Pgn } from '@chesslablab/js-utils';
+import BaseComponent from '../../BaseComponent.js';
 import { binaryWebSocket } from '../../websockets/binary/BinaryWebSocket.js';
 import { stockfishWebSocket } from '../../websockets/game/StockfishWebSocket.js';
+
+class CheckmateForm extends BaseComponent {
+  _checkmateTypes = ['QR,R', 'Q', 'R', 'BB', 'BN'];
+
+  mount() {
+    this.el.addEventListener('submit', event => {
+      event.preventDefault();
+      const formData = new FormData(this.el);
+      const checkmateType = formData.get('items') === 'rand'
+        ? this._checkmateTypes[Math.floor(Math.random() * this._checkmateTypes.length)]
+        : formData.get('items');
+      const split = checkmateType.split(',');
+      stockfishWebSocket.send('/randomizer', {
+        turn: formData.get('color'),
+        items: split.length === 2
+          ? {
+              [formData.get('color')]: split[0],
+              [formData.get('color') === Pgn.symbol.WHITE ? Pgn.symbol.BLACK : Pgn.symbol.WHITE]: split[1]
+            }
+          : {
+              [formData.get('color')]: split[0]
+          }
+      });
+    });
+  }
+}
+
+class EndgameForm extends BaseComponent {
+  _endgameTypes = ['P'];
+
+  mount() {
+    this.el.addEventListener('submit', event => {
+      event.preventDefault();
+      const formData = new FormData(this.el);
+      const checkmateType = formData.get('items') === 'rand'
+        ? this._endgameTypes[Math.floor(Math.random() * this._endgameTypes.length)]
+        : formData.get('items');
+      const split = checkmateType.split(',');
+      stockfishWebSocket.send('/randomizer', {
+        turn: formData.get('color'),
+        items: split.length === 2
+          ? {
+              [formData.get('color')]: split[0],
+              [formData.get('color') === Pgn.symbol.WHITE ? Pgn.symbol.BLACK : Pgn.symbol.WHITE]: split[1]
+            }
+          : {
+              [formData.get('color')]: split[0]
+          }
+      });
+    });
+  }
+}
+
+const checkmateForm = new CheckmateForm({
+  el: document.querySelector('#checkmateForm')
+});
+
+const endgameForm = new EndgameForm({
+  el: document.querySelector('#endgameForm')
+});
 
 try {
   await Promise.all([
