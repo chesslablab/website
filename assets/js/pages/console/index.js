58dfed825c4e9e5b1238c29972b032540a85cfb3
standard <info@programarivm.com>
2025-01-16 18:45:13 +0100

    Updated the console

--- assets/js/pages/console/index.js
+++ assets/js/pages/console/index.js
@@ -1,5 +1,5 @@
-import { consoleForm } from './ConsoleForm.js';
 import AbstractWebSocket from '../../AbstractWebSocket.js';
+import BaseComponent from '../../BaseComponent.js';
 import * as connect from '../../connect.js';
 
 class ConsoleWebSocket extends AbstractWebSocket {
@@ -28,10 +28,71 @@ class ConsoleWebSocket extends AbstractWebSocket {
   }
 }
 
-export const dataWebSocket = new ConsoleWebSocket();
-export const gameWebSocket = new ConsoleWebSocket();
-export const binaryWebSocket = new ConsoleWebSocket();
-export const authWebSocket = new ConsoleWebSocket();
+class ConsoleForm extends BaseComponent {
+  current = 0;
+  stack = [];
+  mount() {
+    this.props.command.addEventListener('keydown', async (event) => {
+      if (event.keyCode === 13) {
+        let filtered = event.target.value.trim();
+        if (filtered.length > 0) {
+          let command = filtered.split(' ')[0];
+          let params = null;
+          try {
+            params = JSON.parse(filtered.substr(filtered.indexOf(' ') + 1));
+          } catch {}
+          if (dataWebSocket.cli.data.includes(command)) {
+            dataWebSocket.send(command, params);
+          } else if (dataWebSocket.cli.game.includes(command)) {
+            gameWebSocket.send(command, params);
+          } else if (dataWebSocket.cli.binary.includes(command)) {
+            binaryWebSocket.send(command, params);
+          } else {
+            authWebSocket.send(command, params);
+          }
+        } else {
+          this.print('\n');
+        }
+      } else if (event.keyCode === 38) {
+        if (this.stack.length > 0) {
+          if (this.current === 0) {
+            this.props.command.value = this.stack[this.current];
+          } else if (this.current > 0) {
+            this.props.command.value = this.stack[this.current];
+            this.current -= 1;
+          }
+        }
+      } else if (event.keyCode === 40) {
+        if (this.current === this.stack.length - 1) {
+          this.props.command.value = this.stack[this.current];
+        } else if (this.current < this.stack.length) {
+          this.props.command.value = this.stack[this.current];
+          this.current += 1;
+        }
+      }
+    });
+  }
+
+  print(msg) {
+    const filtered = this.props.command.value.replace(/(\r\n|\n|\r)/gm,'');
+    const commandP = document.createElement('p');
+    const responseP = document.createElement('p');
+    commandP.appendChild(document.createTextNode(filtered));
+    responseP.appendChild(document.createTextNode(msg));
+    this.props.response.appendChild(commandP);
+    this.props.response.appendChild(responseP);
+    this.props.command.value = '';
+    if (msg !== '\n') {
+      this.stack.push(filtered);
+      this.current = this.stack.length - 1;
+    }
+  }
+}
+
+const dataWebSocket = new ConsoleWebSocket();
+const gameWebSocket = new ConsoleWebSocket();
+const binaryWebSocket = new ConsoleWebSocket();
+const authWebSocket = new ConsoleWebSocket();
 
 try {
   await Promise.all([
@@ -41,5 +102,15 @@ try {
     authWebSocket.connect(6443)
   ]);
 } catch {}
+
+const consoleForm = new ConsoleForm({
+  el: document.querySelector('#consoleForm'),
+  props() {
+    return({
+      command: this.el.querySelector('textarea[name="command"]'),
+      response: this.el.querySelector('div[id="response"]')
+    });
+  }
+});
 
 consoleForm.props.command.focus();
